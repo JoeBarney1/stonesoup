aba5af651e6f5d2e96b2366076293ba345dcf4f7
Steven Hiscocks <sdhiscocks@dstl.gov.uk>
2018-06-20 14:16:43 +0100

    Update hypothesis type test for new Prediction classes

--- stonesoup/types/tests/test_hypothesis.py
+++ stonesoup/types/tests/test_hypothesis.py
@@ -3,15 +3,14 @@ from copy import deepcopy
 import numpy as np
 import pytest
 
-from ..detection import Detection, MissedDetection
-from ..hypothesis import (SingleHypothesis, SingleDistanceHypothesis, SingleProbabilityHypothesis,
-                          JointHypothesis, ProbabilityJointHypothesis, DistanceJointHypothesis,
-                          CompositeHypothesis, CompositeProbabilityHypothesis)
-from ..prediction import StatePrediction, StateMeasurementPrediction
+from ..prediction import Prediction, MeasurementPrediction
+from ..detection import Detection
 from ..track import Track
+from ..hypothesis import (
+    Hypothesis, DistanceHypothesis, JointHypothesis, DistanceJointHypothesis)
 
-prediction = StatePrediction(np.array([[1], [0]]))
-measurement_prediction = StateMeasurementPrediction(np.array([[1], [0]]))
+prediction = Prediction(np.array([[1], [0]]))
+measurement_prediction = MeasurementPrediction(np.array([[1], [0]]))
 detection = Detection(np.array([[1], [0]]))
 distance = float(1)
 
@@ -19,34 +18,19 @@ distance = float(1)
 def test_single_hypothesis():
     """Single Measurement Hypothesis type test"""
 
-    hypothesis = SingleHypothesis(prediction, detection)
-    assert hypothesis.prediction is prediction
-    assert hypothesis.measurement is detection
-    assert hypothesis.measurement_prediction is None
-    assert hypothesis
-
-    hypothesis = SingleHypothesis(prediction, detection,
-                                  measurement_prediction)
-    assert hypothesis.prediction is prediction
-    assert hypothesis.measurement is detection
-    assert hypothesis.measurement_prediction is measurement_prediction
-    assert hypothesis
-
-    hypothesis = SingleHypothesis(prediction, None)
-    assert hypothesis.prediction is prediction
-    assert hypothesis.measurement is None
-    assert hypothesis.measurement_prediction is None
-    assert not hypothesis
+    with pytest.raises(TypeError):
+        Hypothesis(prediction, measurement_prediction, detection)
 
 
 def test_single_distance_hypothesis():
     """Single Measurement Distance Hypothesis type test"""
 
-    hypothesis = SingleDistanceHypothesis(
-        prediction, detection, distance, measurement_prediction)
+    hypothesis = DistanceHypothesis(
+        prediction, measurement_prediction, detection, distance)
 
     assert hypothesis.prediction is prediction
-    assert hypothesis.measurement is detection
+    assert hypothesis.measurement_prediction is measurement_prediction
+    assert hypothesis.detection is detection
     assert hypothesis.distance is distance
     assert hypothesis.measurement_prediction is measurement_prediction
     assert hypothesis.weight == 1/distance
@@ -58,10 +42,10 @@ def test_single_distance_hypothesis():
 def test_single_distance_hypothesis_comparison():
     """Single Measurement Distance Hypothesis comparison test"""
 
-    h1 = SingleDistanceHypothesis(
-        prediction, detection, distance, measurement_prediction)
-    h2 = SingleDistanceHypothesis(
-        prediction, detection, distance + 1, measurement_prediction)
+    h1 = DistanceHypothesis(
+        prediction, measurement_prediction, detection, distance)
+    h2 = DistanceHypothesis(
+        prediction, measurement_prediction, detection, distance + 1)
 
     assert h1 > h2
     assert h2 < h1
@@ -92,10 +76,10 @@ def test_probability_joint_hypothesis():
 
     t1 = Track()
     t2 = Track()
-    h1 = SingleProbabilityHypothesis(
-        prediction, detection, 0.9, measurement_prediction)
-    h2 = SingleProbabilityHypothesis(
-        prediction, detection, 0.1, measurement_prediction)
+    h1 = DistanceHypothesis(
+        prediction, measurement_prediction, detection, distance)
+    h2 = DistanceHypothesis(
+        prediction, measurement_prediction, detection, distance)
 
     hypotheses = {t1: h1, t2: h2}
     joint_hypothesis = JointHypothesis(hypotheses)
@@ -104,27 +88,26 @@ def test_probability_joint_hypothesis():
                       ProbabilityJointHypothesis)
     assert joint_hypothesis[t1] is h1
     assert joint_hypothesis[t2] is h2
-    assert joint_hypothesis.probability == h1.probability * h2.probability
+    assert joint_hypothesis.distance == distance * 2
 
 
-def test_probability_joint_hypothesis_comparison():
-    """Probability Joint Hypothesis comparison test"""
+def test_distance_joint_hypothesis_comparison():
+    """Distance Joint Hypothesis comparison test"""
 
     t1 = Track()
     t2 = Track()
-    h1 = SingleProbabilityHypothesis(
-        prediction, detection, 0.75, measurement_prediction)
-    h2 = SingleProbabilityHypothesis(
-        prediction, detection, 0.75, measurement_prediction)
-    h3 = SingleProbabilityHypothesis(
-        prediction, detection, 0.25, measurement_prediction)
+    h1 = DistanceHypothesis(
+        prediction, measurement_prediction, detection, distance)
+    h2 = DistanceHypothesis(
+        prediction, measurement_prediction, detection, distance)
+    h3 = DistanceHypothesis(
+        prediction, measurement_prediction, detection, distance + 1)
 
     hypotheses1 = {t1: h1, t2: h2}
     hypotheses2 = {t1: h1, t2: h3}
     j1 = JointHypothesis(hypotheses1)
     j1.normalise()
     j2 = JointHypothesis(hypotheses2)
-    j2.normalise()
 
     assert j1 > j2
     assert j2 < j1
