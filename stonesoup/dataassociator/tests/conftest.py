fa77d01f826cd371db809246aa74e6f387fd95c6
cjengland <cjengland@dstl.gov.uk>
2018-06-27 15:34:54 +0100

    Added tests for hypothesiser, associator, and ground truth simulator.

--- stonesoup/dataassociator/tests/conftest.py
+++ stonesoup/dataassociator/tests/conftest.py
@@ -1,42 +1,32 @@
+# -*- coding: utf-8 -*-
 import pytest
-import numpy as np
 
-from ...hypothesiser.probability import PDAHypothesiser
-from ...hypothesiser.distance import DistanceHypothesiser
-from ...measures import Mahalanobis
-from ...models.measurement.linear import LinearGaussian
-from ...types.array import CovarianceMatrix
-from ...models.transition.linear import ConstantVelocity, CombinedLinearGaussianTransitionModel
-from ...predictor.kalman import KalmanPredictor
-from ...updater.kalman import ExtendedKalmanUpdater
+from ...types import DistanceHypothesis, GaussianStatePrediction,\
+    GaussianMeasurementPrediction
 
 
 @pytest.fixture()
-def measurement_model():
-    return LinearGaussian(ndim_state=4, mapping=[0, 2],
-                          noise_covar=CovarianceMatrix(np.diag([1, 1])))
+def hypothesiser():
+    class TestGaussianHypothesiser:
+        def hypothesise(self, track, detections, timestamp):
+            hypotheses = list()
+            for detection in detections:
+                prediction = GaussianStatePrediction(track.state_vector + 1,
+                                                     track.covar * 2,
+                                                     detection.timestamp)
+                measurement_prediction =\
+                    GaussianMeasurementPrediction(prediction.state_vector,
+                                                  prediction.covar,
+                                                  prediction.timestamp)
+                distance = abs(track.state_vector - detection.state_vector)
 
+                hypotheses.append(DistanceHypothesis(prediction,
+                                                     measurement_prediction,
+                                                     detection, distance))
 
-@pytest.fixture()
-def predictor():
-    transition_model = CombinedLinearGaussianTransitionModel([ConstantVelocity(1),
-                                                              ConstantVelocity(1)])
-    return KalmanPredictor(transition_model)
-
-
-@pytest.fixture()
-def updater(measurement_model):
-    return ExtendedKalmanUpdater(measurement_model)
-
-
-@pytest.fixture()
-def probability_hypothesiser(predictor, updater):
-    return PDAHypothesiser(predictor, updater,
-                           clutter_spatial_density=1.2e-2,
-                           prob_detect=0.9, prob_gate=0.99,
-                           include_all=True)
-
-
-@pytest.fixture()
-def distance_hypothesiser(predictor, updater):
-    return DistanceHypothesiser(predictor, updater, Mahalanobis(), 10)
+            prediction = GaussianStatePrediction(track.state_vector + 1,
+                                                 track.covar * 2, timestamp)
+            hypotheses.append(DistanceHypothesis(prediction, None, None,
+                                                 distance=10))
+            return hypotheses
+    return TestGaussianHypothesiser()
--- stonesoup/dataassociator/tests/test_neighbour.py
+++ stonesoup/dataassociator/tests/test_neighbour.py
@@ -1,32 +1,25 @@
+# -*- coding: utf-8 -*-
 import datetime
 
 import pytest
 import numpy as np
 
-from ..neighbour import (
-    NearestNeighbour, GlobalNearestNeighbour, GNNWith2DAssignment)
-from ...types.detection import Detection
-from ...types.state import GaussianState
-from ...types.track import Track
+from ..neighbour import NearestNeighbour, GlobalNearestNeighbour
+from ...types import Track, Detection, GaussianState
 
 
-@pytest.fixture(params=[
-    NearestNeighbour, GlobalNearestNeighbour, GNNWith2DAssignment])
-def associator(request, distance_hypothesiser):
-    return request.param(distance_hypothesiser)
-
-
-@pytest.fixture(params=[GNNWith2DAssignment])
-def probability_associator(request, probability_hypothesiser):
-    return request.param(probability_hypothesiser)
+@pytest.fixture(params=[NearestNeighbour, GlobalNearestNeighbour])
+def associator(request, hypothesiser):
+    return request.param(hypothesiser)
 
 
 def test_nearest_neighbour(associator):
+
     timestamp = datetime.datetime.now()
-    t1 = Track([GaussianState(np.array([[0, 0, 0, 0]]), np.diag([1, 0.1, 1, 0.1]), timestamp)])
-    t2 = Track([GaussianState(np.array([[3, 0, 3, 0]]), np.diag([1, 0.1, 1, 0.1]), timestamp)])
-    d1 = Detection(np.array([[2, 2]]), timestamp)
-    d2 = Detection(np.array([[5, 5]]), timestamp)
+    t1 = Track([GaussianState(np.array([[0]]), np.array([[1]]), timestamp)])
+    t2 = Track([GaussianState(np.array([[3]]), np.array([[1]]), timestamp)])
+    d1 = Detection(np.array([[2]]))
+    d2 = Detection(np.array([[5]]))
 
     tracks = {t1, t2}
     detections = {d1, d2}
@@ -37,17 +30,18 @@ def test_nearest_neighbour(associator):
     assert len(associations) == 2
 
     # Each track should associate with a unique detection
-    associated_measurements = [hypothesis.measurement
-                               for hypothesis in associations.values()
-                               if hypothesis.measurement]
-    assert len(associated_measurements) == len(set(associated_measurements))
+    associated_detections = [hypothesis.detection
+                             for hypothesis in associations.values()
+                             if hypothesis.detection is not None]
+    assert len(associated_detections) == len(set(associated_detections))
 
 
 def test_missed_detection_nearest_neighbour(associator):
+
     timestamp = datetime.datetime.now()
-    t1 = Track([GaussianState(np.array([[0, 0, 0, 0]]), np.diag([1, 0.1, 1, 0.1]), timestamp)])
-    t2 = Track([GaussianState(np.array([[3, 0, 3, 0]]), np.diag([1, 0.1, 1, 0.1]), timestamp)])
-    d1 = Detection(np.array([[20, 20]]), timestamp)
+    t1 = Track([GaussianState(np.array([[0]]), np.array([[1]]), timestamp)])
+    t2 = Track([GaussianState(np.array([[3]]), np.array([[1]]), timestamp)])
+    d1 = Detection(np.array([[20]]))
 
     tracks = {t1, t2}
     detections = {d1}
@@ -55,28 +49,5 @@ def test_missed_detection_nearest_neighbour(associator):
     associations = associator.associate(tracks, detections, timestamp)
 
     # Best hypothesis should be missed detection hypothesis
-    assert all(not hypothesis.measurement
+    assert all(hypothesis.detection is None
                for hypothesis in associations.values())
-
-
-def test_probability_gnn(probability_associator):
-    timestamp = datetime.datetime.now()
-    t1 = Track([GaussianState(np.array([[0, 0, 0, 0]]), np.diag([1, 0.1, 1, 0.1]), timestamp)])
-    t2 = Track([GaussianState(np.array([[3, 0, 3, 0]]), np.diag([1, 0.1, 1, 0.1]), timestamp)])
-    d1 = Detection(np.array([[2, 2]]), timestamp)
-    d2 = Detection(np.array([[5, 5]]), timestamp)
-
-    tracks = {t1, t2}
-    detections = {d1, d2}
-
-    associations = probability_associator.associate(
-        tracks, detections, timestamp)
-
-    # There should be 2 associations
-    assert len(associations) == 2
-
-    # Each track should associate with a unique detection
-    associated_measurements = [hypothesis.measurement
-                               for hypothesis in associations.values()
-                               if hypothesis.measurement]
-    assert len(associated_measurements) == len(set(associated_measurements))
