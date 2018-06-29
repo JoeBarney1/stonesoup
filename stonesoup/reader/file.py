850f192412f902646f1387a6dc0dd3257e0f5067
Steven Hiscocks <sdhiscocks@dstl.gov.uk>
2018-06-29 13:15:18 +0100

    Add context manager methods to file reader

--- stonesoup/reader/file.py
+++ stonesoup/reader/file.py
@@ -13,6 +13,17 @@ class FileReader(Reader):
         if not isinstance(path, Path):
             path = Path(path)  # Ensure Path
         super().__init__(path, *args, **kwargs)
+        self._file = None
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        if getattr(self, '_file'):
+            self._file.close()
+
+    def __del__(self):
+        self.__exit__()
 
 
 class BinaryFileReader(FileReader):
