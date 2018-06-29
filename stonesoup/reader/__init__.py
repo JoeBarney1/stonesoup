2b0817d126fb15b6f9088a0a718e70b8bec39d2b
Steven Hiscocks <sdhiscocks@dstl.gov.uk>
2018-06-29 13:14:11 +0100

    Create YAML Detection reader and writer

--- stonesoup/reader/__init__.py
+++ stonesoup/reader/__init__.py
@@ -1,5 +1,9 @@
 """Reader classes are used for getting data into the framework."""
 from .base import Reader, DetectionReader, GroundTruthReader, SensorDataReader
 
-__all__ = [
-    'Reader', 'DetectionReader', 'GroundTruthReader', 'SensorDataReader']
+from .generic import *  # noqa:F401,F403
+from .file import *  # noqa:F401,F403
+from .yaml import *  # noqa:F401,F403
+
+__all__ = ['DetectionReader', 'GroundTruthReader', 'SensorDataReader']
+__all__.extend(subclass_.__name__ for subclass_ in Reader.subclasses)
--- stonesoup/writer/__init__.py
+++ stonesoup/writer/__init__.py
@@ -1,3 +1,4 @@
 from .base import Writer, MetricsWriter, TrackWriter
+from .yaml import *  # noqa:F401,F403
 
 __all__ = ['Writer', 'MetricsWriter', 'TrackWriter']
