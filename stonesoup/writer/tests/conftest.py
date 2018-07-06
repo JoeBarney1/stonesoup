e078748cc68f78d1d94a4dc21349e6e241ef5af7
Steven Hiscocks <sdhiscocks@dstl.gov.uk>
2018-07-06 17:41:09 +0100

    Modify YAMLDetectionReader/Writer to generic YAMLReader/Writer

--- stonesoup/writer/tests/conftest.py
+++ stonesoup/writer/tests/conftest.py
@@ -1,10 +1,8 @@
+# -*- coding: utf-8 -*-
 import datetime
 
 import pytest
 
-from ...buffered_generator import BufferedGenerator
-from ...reader import DetectionReader, GroundTruthReader
-from ...tracker import Tracker
 from ...types.array import StateVector
 from ...types.detection import Detection
 from ...types.groundtruth import GroundTruthState, GroundTruthPath
@@ -14,62 +12,59 @@ from ...types.track import Track
 
 @pytest.fixture()
 def detection_reader():
-    class TestDetectionReader(DetectionReader):
-        @BufferedGenerator.generator_method
+    class TestDetectionReader():
+        def __init__(self):
+            self.detections = set()
+
         def detections_gen(self):
             time = datetime.datetime(2018, 1, 1, 14)
             state_vector = StateVector([[0]])
             for i in range(3):
-                detections = {
+                self.detections = {
                     Detection(state_vector + i,  timestamp=time)
                     for _ in range(i)}
-                yield time, detections
+                yield time, self.detections
                 time += datetime.timedelta(minutes=1)
     return TestDetectionReader()
 
 
 @pytest.fixture()
 def groundtruth_reader():
-    class TestGroundTruthReader(GroundTruthReader):
-        @BufferedGenerator.generator_method
+    class TestGroundTruthReader():
+        def __init__(self):
+            self.groundtruth_paths = set()
+
         def groundtruth_paths_gen(self):
             time = datetime.datetime(2018, 1, 1, 14)
             state_vector = StateVector([[0]])
-            for i in range(2):
-                groundtruth_paths = {
+            for i in range(3):
+                self.groundtruth_paths = {
                     GroundTruthPath(
                         [GroundTruthState(
                             state_vector + i + 10*j, timestamp=time)
-                         for j in range(i)],
-                        str(k))
-                    for k in range(i)}
-                yield time, groundtruth_paths
+                         for j in range(i)])
+                    for _ in range(i)}
+                yield time, self.groundtruth_paths
                 time += datetime.timedelta(minutes=1)
     return TestGroundTruthReader()
 
 
 @pytest.fixture()
 def tracker():
-    class TestTracker(Tracker):
-        @property
-        def tracks(self):
-            return self._tracks
+    class TestTracker():
+        def __init__(self):
+            self.tracks = set()
 
-        def __iter__(self):
-            self.iter = iter(range(2))
-            self.time = datetime.datetime(2018, 1, 1, 13, 59)
-            return super().__iter__()
-
-        def __next__(self):
-            i = next(self.iter)
+        def tracks_gen(self):
+            time = datetime.datetime(2018, 1, 1, 14)
             state_vector = StateVector([[0]])
-            self.time += datetime.timedelta(minutes=1)
-            self._tracks = {
-                Track(
-                    [State(
-                        state_vector + i + 10*j, timestamp=self.time)
-                        for j in range(i)],
-                    str(k))
-                for k in range(i)}
-            return self.time, self.tracks
+            for i in range(3):
+                self.tracks = {
+                    Track(
+                        [State(
+                            state_vector + i + 10*j, timestamp=time)
+                            for j in range(i)])
+                    for _ in range(i)}
+                yield time, self.tracks
+                time += datetime.timedelta(minutes=1)
     return TestTracker()
--- stonesoup/writer/tests/test_yaml.py
+++ stonesoup/writer/tests/test_yaml.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from textwrap import dedent
 
 import pytest
@@ -8,11 +9,10 @@ from ..yaml import YAMLWriter
 def test_detections_yaml(detection_reader, tmpdir):
     filename = tmpdir.join("detections.yaml")
 
-    with YAMLWriter(filename.strpath,
-                    detections_source=detection_reader) as writer:
+    with YAMLWriter(filename, detections_source=detection_reader) as writer:
         writer.write()
 
-    with filename.open('r') as yaml_file:
+    with open(filename, 'r') as yaml_file:
         generated_yaml = yaml_file.read()
 
     expected_yaml = dedent("""\
@@ -24,26 +24,23 @@ def test_detections_yaml(detection_reader, tmpdir):
          time: &id001 2018-01-01 14:01:00
          detections: !!set
            ? !stonesoup.types.detection.Detection
-           - state_vector: !stonesoup.types.array.StateVector
+           - state_vector: !numpy.ndarray
              - [1]
            - timestamp: *id001
-           - metadata: {}
            :
          ...
          ---
          time: &id001 2018-01-01 14:02:00
          detections: !!set
            ? !stonesoup.types.detection.Detection
-           - state_vector: !stonesoup.types.array.StateVector
+           - state_vector: !numpy.ndarray
              - [2]
            - timestamp: *id001
-           - metadata: {}
            :
            ? !stonesoup.types.detection.Detection
-           - state_vector: !stonesoup.types.array.StateVector
+           - state_vector: !numpy.ndarray
              - [2]
            - timestamp: *id001
-           - metadata: {}
            :
          ...
          """)
@@ -54,11 +51,10 @@ def test_detections_yaml(detection_reader, tmpdir):
 def test_groundtruth_paths_yaml(groundtruth_reader, tmpdir):
     filename = tmpdir.join("groundtruth_paths.yaml")
 
-    with YAMLWriter(filename.strpath,
-                    groundtruth_source=groundtruth_reader) as writer:
+    with YAMLWriter(filename, groundtruth_source=groundtruth_reader) as writer:
         writer.write()
 
-    with filename.open('r') as yaml_file:
+    with open(filename, 'r') as yaml_file:
         generated_yaml = yaml_file.read()
 
     expected_yaml = dedent("""\
@@ -72,11 +68,35 @@ def test_groundtruth_paths_yaml(groundtruth_reader, tmpdir):
           ? !stonesoup.types.groundtruth.GroundTruthPath
           - states:
             - !stonesoup.types.groundtruth.GroundTruthState
-              - state_vector: !stonesoup.types.array.StateVector
+              - state_vector: !numpy.ndarray
                 - [1]
               - timestamp: *id001
-              - metadata: {}
-          - id: '0'
+          :
+        ...
+        ---
+        time: &id001 2018-01-01 14:02:00
+        groundtruth_paths: !!set
+          ? !stonesoup.types.groundtruth.GroundTruthPath
+          - states:
+            - !stonesoup.types.groundtruth.GroundTruthState
+              - state_vector: !numpy.ndarray
+                - [2]
+              - timestamp: *id001
+            - !stonesoup.types.groundtruth.GroundTruthState
+              - state_vector: !numpy.ndarray
+                - [12]
+              - timestamp: *id001
+          :
+          ? !stonesoup.types.groundtruth.GroundTruthPath
+          - states:
+            - !stonesoup.types.groundtruth.GroundTruthState
+              - state_vector: !numpy.ndarray
+                - [2]
+              - timestamp: *id001
+            - !stonesoup.types.groundtruth.GroundTruthState
+              - state_vector: !numpy.ndarray
+                - [12]
+              - timestamp: *id001
           :
         ...
         """)
@@ -87,10 +107,10 @@ def test_groundtruth_paths_yaml(groundtruth_reader, tmpdir):
 def test_tracks_yaml(tracker, tmpdir):
     filename = tmpdir.join("tracks.yaml")
 
-    with YAMLWriter(filename.strpath, tracks_source=tracker) as writer:
+    with YAMLWriter(filename, tracks_source=tracker) as writer:
         writer.write()
 
-    with filename.open('r') as yaml_file:
+    with open(filename, 'r') as yaml_file:
         generated_yaml = yaml_file.read()
 
     expected_yaml = dedent("""\
@@ -104,10 +124,35 @@ def test_tracks_yaml(tracker, tmpdir):
           ? !stonesoup.types.track.Track
           - states:
             - !stonesoup.types.state.State
-              - state_vector: !stonesoup.types.array.StateVector
+              - state_vector: !numpy.ndarray
                 - [1]
               - timestamp: *id001
-          - id: '0'
+          :
+        ...
+        ---
+        time: &id001 2018-01-01 14:02:00
+        tracks: !!set
+          ? !stonesoup.types.track.Track
+          - states:
+            - !stonesoup.types.state.State
+              - state_vector: !numpy.ndarray
+                - [2]
+              - timestamp: *id001
+            - !stonesoup.types.state.State
+              - state_vector: !numpy.ndarray
+                - [12]
+              - timestamp: *id001
+          :
+          ? !stonesoup.types.track.Track
+          - states:
+            - !stonesoup.types.state.State
+              - state_vector: !numpy.ndarray
+                - [2]
+              - timestamp: *id001
+            - !stonesoup.types.state.State
+              - state_vector: !numpy.ndarray
+                - [12]
+              - timestamp: *id001
           :
         ...
         """)
@@ -118,4 +163,4 @@ def test_tracks_yaml(tracker, tmpdir):
 def test_yaml_bad_init(tmpdir):
     filename = tmpdir.join("bad_init.yaml")
     with pytest.raises(ValueError, match="At least one source required"):
-        YAMLWriter(filename.strpath)
+        YAMLWriter(filename)
