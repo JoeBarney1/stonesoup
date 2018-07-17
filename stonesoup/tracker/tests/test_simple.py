28565d3484d4a4ebd404b462a562389a90810530
Steven Hiscocks <sdhiscocks@dstl.gov.uk>
2018-07-17 14:51:14 +0100

    Add test cases for tracker and fix single target tracks return
    In single target tracker, a set with `None` was returned rather than
    empty set when track didn't exist.

--- stonesoup/tracker/tests/test_simple.py
+++ stonesoup/tracker/tests/test_simple.py
@@ -1,6 +1,7 @@
+# -*- coding: utf-8 -*-
 import datetime
-from ..simple import SingleTargetTracker, MultiTargetTracker, \
-    MultiTargetMixtureTracker, SingleTargetMixtureTracker
+
+from ..simple import SingleTargetTracker, MultiTargetTracker
 
 
 def test_single_target_tracker(
@@ -10,7 +11,7 @@ def test_single_target_tracker(
 
     previous_time = datetime.datetime(2018, 1, 1, 13, 59)
     total_tracks = set()
-    for time, tracks in tracker:
+    for time, tracks in tracker.tracks_gen():
         assert time == previous_time + datetime.timedelta(minutes=1)
         assert len(tracks) <= 1  # Shouldn't have more than one track
         for track in tracks:
@@ -22,24 +23,6 @@ def test_single_target_tracker(
     assert len(total_tracks) >= 2  # Should of had at least 2 over all steps
 
 
-def test_single_target_mixture_tracker(
-        initiator, deleter, detector, data_mixture_associator, updater):
-    tracker = SingleTargetMixtureTracker(
-        initiator, deleter, detector, data_mixture_associator, updater)
-
-    previous_time = datetime.datetime(2018, 1, 1, 13, 59)
-    total_tracks = set()
-    for time, tracks in tracker:
-        assert time == previous_time + datetime.timedelta(minutes=1)
-        assert len(tracks) <= 1  # Shouldn't have more than one track
-        for track in tracks:
-            assert len(track.states) <= 10  # Deleter should delete them
-        total_tracks |= tracks
-
-        previous_time = time
-    assert len(total_tracks) >= 2
-
-
 def test_multi_target_tracker(
         initiator, deleter, detector, data_associator, updater):
     tracker = MultiTargetTracker(
@@ -48,7 +31,7 @@ def test_multi_target_tracker(
     previous_time = datetime.datetime(2018, 1, 1, 13, 59)
     max_tracks = 0
     total_tracks = set()
-    for time, tracks in tracker:
+    for time, tracks in tracker.tracks_gen():
         assert time == previous_time + datetime.timedelta(minutes=1)
         assert len(tracks) <= 3  # Shouldn't have more than three tracks
         for track in tracks:
@@ -57,33 +40,6 @@ def test_multi_target_tracker(
             sorted_vectors = sorted(track.state_vector for track in tracks)
             assert sorted_vectors[0] + 10 == sorted_vectors[1]
             assert sorted_vectors[1] + 10 == sorted_vectors[2]
-
-        max_tracks = max(max_tracks, len(tracks))
-        total_tracks |= tracks
-
-        previous_time = time
-
-    assert max_tracks >= 3  # Should of had at least 3 tracks in single step
-    assert len(total_tracks) >= 6  # Should of had at least 6 over all steps
-
-
-def test_multi_target_mixture_tracker(
-        initiator, deleter, detector, data_mixture_associator, updater):
-    tracker = MultiTargetMixtureTracker(
-        initiator, deleter, detector, data_mixture_associator, updater)
-
-    previous_time = datetime.datetime(2018, 1, 1, 13, 59)
-    max_tracks = 0
-    total_tracks = set()
-    for time, tracks in tracker:
-        assert time == previous_time + datetime.timedelta(minutes=1)
-        # assert len(tracks) <= 3  # Shouldn't have more than three tracks
-        for track in tracks:
-            assert len(track.states) <= 10  # Deleter should delete these
-        # if len(tracks) == 3:
-        #    sorted_vectors = sorted(track.state_vector for track in tracks)
-        #    assert sorted_vectors[0] + 10 == sorted_vectors[1]
-        #    assert sorted_vectors[1] + 10 == sorted_vectors[2]
 
         max_tracks = max(max_tracks, len(tracks))
         total_tracks |= tracks
