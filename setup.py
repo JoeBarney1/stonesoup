582abefa2e1e394c350b24ce36f3c0628ab2ddda
Steven Hiscocks <sdhiscocks@dstl.gov.uk>
2018-07-26 16:11:21 +0100

    Add CircleCI config and update setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+from stonesoup import __version__ as version
+
+setup(name='stonesoup',
+      version=version,
+      description='A target tracking development/testing framework',
+      url='https://github.com/dstl/Stone-Soup',
+      packages=find_packages(exclude=('docs', '*.tests')),
+      install_requires=['ruamel.yaml', 'scipy', 'matplotlib'],
+      extras_require={
+          'dev': [
+              'pytest-flake8', 'Sphinx', 'sphinx_rtd_theme', 'coverage'],
+      },
+      )
