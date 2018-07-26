978e07dfe8fa817e95173edd609f92c25843ed6c
Steven Hiscocks <sdhiscocks@dstl.gov.uk>
2018-07-26 18:28:56 +0100

    Fix issue in Python 3.5 with `open` not accepting `PathLike` objects

--- stonesoup/reader/generic.py
+++ stonesoup/reader/generic.py
@@ -134,9 +134,16 @@ class CSVDetectionReader(DetectionReader, _CSVReader):
 
     def detections_gen(self):
         with self.path.open(encoding=self.encoding, newline='') as csv_file:
-            detections = set()
-            previous_time = None
-            for row in csv.DictReader(csv_file, **self.csv_options):
+            reader = csv.DictReader(csv_file)
+            for row in reader:
+                if self.time_field_format is not None:
+                    time_field_value = datetime.strptime(
+                        row[self.time_field], self.time_field_format)
+                elif self.time_field is True:
+                    time_field_value = datetime.fromtimestamp(
+                        row[self.time_field])
+                else:
+                    time_field_value = parse(row[self.time_field])
 
                 detect = Detection(np.array(
                     [[row[col_name]] for col_name in self.state_vector_fields],
