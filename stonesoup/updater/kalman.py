254947764c31a451a560efdf29e65e88e27928a8
sglvladi <sglvladi@gmail.com>
2019-01-13 21:44:32 +0200

    Fix small bug in ExtendedKalmanUpdater.get_measurement_prediction()

--- stonesoup/updater/kalman.py
+++ stonesoup/updater/kalman.py
@@ -654,7 +654,8 @@ class IteratedKalmanUpdater(ExtendedKalmanUpdater):
         if measurement_model is None:
             measurement_model = self.measurement_model
         measurement_matrix, measurement_noise_covar, measurement_function = \
-            self._extract_model_parameters(measurement_model)
+            self._extract_model_parameters(measurement_model,
+                                           state_prediction.state_vector)
 
         meas_pred_mean, meas_pred_covar, cross_covar = \
             self.get_measurement_prediction_lowlevel(state_prediction.mean,
@@ -663,16 +664,6 @@ class IteratedKalmanUpdater(ExtendedKalmanUpdater):
                                                      measurement_matrix,
                                                      measurement_noise_covar)
 
-        return GaussianMeasurementPrediction(meas_pred_mean, meas_pred_covar,
-                                             state_prediction.timestamp,
-                                             cross_covar)
-
-        meas_pred_mean, meas_pred_covar, cross_covar = \
-            self.get_measurement_prediction_lowlevel(state_prediction.mean,
-                                                     state_prediction.covar,
-                                                     measurement_function,
-                                                     measurement_matrix,
-                                                     measurement_noise_covar)
         return GaussianMeasurementPrediction(meas_pred_mean, meas_pred_covar,
                                              state_prediction.timestamp,
                                              cross_covar)
