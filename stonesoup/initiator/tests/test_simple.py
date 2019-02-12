a447363c57948b1e5463e3908504e77ef4527c48
sglvladi <sglvladi@gmail.com>
2019-02-12 14:41:24 +0000

    Removed print statements from Singer tests + minor corrections

--- stonesoup/initiator/tests/test_simple.py
+++ stonesoup/initiator/tests/test_simple.py
@@ -63,9 +63,6 @@ def test_spi(measurement_model):
 
         # Compare against both tracks
         for track_idx, track in enumerate(tracks):
-            print(eval_track_state.covar-track.covar)
-            print(np.array_equal(eval_track_state.covar, track.covar))
-            print("---------")
 
             if(np.array_equal(eval_track_state.mean, track.mean)
                and np.array_equal(eval_track_state.covar, track.covar)):
@@ -73,9 +70,6 @@ def test_spi(measurement_model):
             if (np.array_equal(eval_track_state.mean, track.mean)
                     and np.array_equal(eval_track_state.covar, track.covar)):
                 evaluated_tracks[track_idx] = True
-
-    print(evaluated_tracks)
-    # print(tracks)
 
     # Ensure both tracks have been evaluated
     assert(all(evaluated_tracks))
