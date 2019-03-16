0af589a30d806d0ef4d71107989050f5e92258bc
Steven Hiscocks <sdhiscocks@dstl.gov.uk>
2019-03-16 17:01:22 +0000

    Add Feeders for 2d and 3d geo coordinate transforms
    Include ENU, NED and UTM.

--- stonesoup/feeder/geo.py
+++ stonesoup/feeder/geo.py
@@ -1,20 +1,20 @@
+# -*- coding: utf-8 -*-
 import warnings
 from abc import abstractmethod
-from typing import Tuple
 
 import utm
 from pymap3d import geodetic2enu, geodetic2ned
 
-from .base import DetectionFeeder, GroundTruthFeeder
+from .base import Feeder
 from ..base import Property
-from ..buffered_generator import BufferedGenerator
+from ..types.detection import Detection
 
 
-class _LLARefConverter(DetectionFeeder, GroundTruthFeeder):
-    reference_point: Tuple[float, float, float] = Property(
-        doc="(Long, Lat, Altitude)")
-    mapping: Tuple[int, int, int] = Property(
-        default=(0, 1, 2),
+class _LLARefConverter(Feeder):
+    reference_point = Property(
+        (float, float, float), doc="(Long, Lat, Altitude)")
+    mapping = Property(
+        (float, float, float), default=(0, 1, 2),
         doc="Indexes of long, lat, altitude. Default (0, 1, 2)")
 
     @property
@@ -22,28 +22,41 @@ class _LLARefConverter(DetectionFeeder, GroundTruthFeeder):
     def _converter(self):
         raise NotImplementedError
 
-    @BufferedGenerator.generator_method
-    def data_gen(self):
-        for time, states in self.reader:
+    @property
+    def detections(self):
+        return self._detections.copy()
 
-            for state in states:
+    def detections_gen(self):
+        for time, detections in self.detector.detections_gen():
+
+            self._detections = set()
+            for detection in detections:
                 new_coord = self._converter(
-                    state.state_vector[self.mapping[1], 0],  # Lat
-                    state.state_vector[self.mapping[0], 0],  # Long
-                    state.state_vector[self.mapping[2], 0],  # Altitude
+                    detection.state_vector[self.mapping[1], 0],  # Lat
+                    detection.state_vector[self.mapping[0], 0],  # Long
+                    detection.state_vector[self.mapping[2], 0],  # Altitude
                     self.reference_point[1],  # Lat
                     self.reference_point[0],  # Long
                     self.reference_point[2],  # Altitude
                 )
-                state.state_vector[self.mapping, 0] = new_coord
-            yield time, states
+                state_vector = detection.state_vector.copy()
+                state_vector[self.mapping, 0] = new_coord
+                self._detections.add(
+                    Detection(
+                        state_vector,
+                        timestamp=detection.timestamp,
+                        measurement_model=detection.measurement_model,
+                        metadata=detection.metadata))
+
+            yield time, self.detections
 
 
 class LLAtoENUConverter(_LLARefConverter):
     """Converts Long., Lat., Alt. to East, North, Up coordinate space.
 
-    This replaces Longitude (°), Latitude (°) and Altitude (m) of a :class:`~.State` with East (m),
-    North (m) and Up (m) coordinate space from a defined :attr:`reference_point`.
+    This replaces Longitude (°), Latitude (°) and Altitude (m) of a
+    :class:`~.Detection` with East (m), North (m) and Up (m) coordinate space
+    from a defined :attr:`reference_point`.
     """
 
     @property
@@ -54,8 +67,9 @@ class LLAtoENUConverter(_LLARefConverter):
 class LLAtoNEDConverter(_LLARefConverter):
     """Converts Long., Lat., Alt. to North, East, Down coordinate space.
 
-    This replaces Longitude (°), Latitude (°) and Altitude (m) of a :class:`~.State` with North
-    (m), East (m) and Down (m) coordinate space from a defined :attr:`reference_point`.
+    This replaces Longitude (°), Latitude (°) and Altitude (m) of a
+    :class:`~.Detection` with North (m), East (m) and Down (m) coordinate space
+    from a defined :attr:`reference_point`.
     """
 
     @property
@@ -63,38 +77,44 @@ class LLAtoNEDConverter(_LLARefConverter):
         return geodetic2ned
 
 
-class LongLatToUTMConverter(DetectionFeeder, GroundTruthFeeder):
+class LongLatToUTMConverter(Feeder):
     """Converts long. and lat. to UTM easting and northing.
 
-    This replaces Longitude (°), Latitude (°) of a :class:`~.State` with East (m), North (m)
-    coordinate space in a Universal Transverse Mercator zone.
+    This replaces Longitude (°), Latitude (°) of a :class:`~.Detection` with
+    East (m), North (m) coordinate space in a Universal Transverse Mercator
+    zone.
 
     Note
     ----
-    Whilst this allows for data to be converted even if it is outside of the set zone, the errors
-    will increase the further the position is from the zone being used.
+    Whilst this allows for detections to be converted even if they are
+    outside of the set zone, the errors will increase the further the
+    position is from the zone being used.
 
     """
 
-    mapping: Tuple[int, int] = Property(
-        default=(0, 1),
+    mapping = Property(
+        (float, float), default=(0, 1),
         doc="Indexes of long, lat. Default (0, 1)")
-    zone_number: int = Property(
-        default=None,
-        doc="UTM zone number to carry out conversion. Default `None`, where it will select the "
-            "zone based on the first yielded data.")
-    northern: bool = Property(
-        default=None,
-        doc="UTM northern for northern or southern grid. Default `None`, where it will be based "
-            "on the first yielded data.")
+    zone_number = Property(
+        int, default=None,
+        doc="UTM zone number to carry out conversion. Default `None`, where it"
+            "will select the zone based on the first detection.")
+    northern = Property(
+        bool, default=None,
+        doc="UTM northern for northern or southern grid. Default `None`, where"
+            "it will be base on the first detection.")
 
-    @BufferedGenerator.generator_method
-    def data_gen(self):
-        for time, states in self.reader:
+    @property
+    def detections(self):
+        return self._detections.copy()
 
-            for state in states:
+    def detections_gen(self):
+        for time, detections in self.detector.detections_gen():
+
+            self._detections = set()
+            for detection in detections:
                 easting, northing, zone_num, northern = utm.from_latlon(
-                    *state.state_vector[self.mapping[::-1], :],
+                    *detection.state_vector[self.mapping[::-1], 0],
                     self.zone_number)
                 if self.zone_number is None:
                     self.zone_number = zone_num
@@ -102,8 +122,16 @@ class LongLatToUTMConverter(DetectionFeeder, GroundTruthFeeder):
                     self.northern = northern >= 'N'
                 elif (self.northern and northern < 'N') or (
                             not self.northern and northern >= 'N'):
-                    warnings.warn("State cannot be converted to UTM zone")
+                    warnings.warn("Detection cannot be converted to UTM zone")
                     continue
 
-                state.state_vector[self.mapping, 0] = easting, northing
-            yield time, states
+                state_vector = detection.state_vector.copy()
+                state_vector[self.mapping, 0] = easting, northing
+                self._detections.add(
+                    Detection(
+                        state_vector,
+                        timestamp=detection.timestamp,
+                        measurement_model=detection.measurement_model,
+                        metadata=detection.metadata))
+
+            yield time, self.detections
