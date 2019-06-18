4c3ac62baa52ad639d2465377bce7a6ec5dbd8b2
Steven Hiscocks <sdhiscocks@dstl.gov.uk>
2019-06-18 13:42:10 +0100

    Create OpenSky Network REST API Reader

--- stonesoup/reader/opensky.py
+++ stonesoup/reader/opensky.py
@@ -1,30 +1,23 @@
+# -*- coding: utf-8 -*-
 import datetime
 from time import sleep
-from typing import Tuple
 
-try:
-    import requests
-    from requests.compat import urljoin
-except ImportError as error:
-    raise ImportError(
-        "Usage of opensky requires the dependency 'requests' is installed. ") from error
+import requests
+from requests.compat import urljoin
 
-
-from .base import Reader, DetectionReader, GroundTruthReader
+from .base import DetectionReader
 from ..base import Property
-from ..buffered_generator import BufferedGenerator
 from ..types.detection import Detection
-from ..types.groundtruth import GroundTruthPath, GroundTruthState
-from ..types.state import State
+from ..types.state import StateVector
 
 
-class _OpenSkyNetworkReader(Reader):
+class OpenSkyNetworkReader(DetectionReader):
     """OpenSky Network reader
 
     This reader uses the `OpenSky Network <https://opensky-network.org/>`_ REST
     API to fetch air traffic control data.
 
-    The state vector consists of longitude, latitude
+    The detection state vector consists of longitude, latitude
     (in decimal degrees) and altitude (in meters).
 
     .. note::
@@ -39,25 +32,31 @@ class _OpenSkyNetworkReader(Reader):
         0: "ADS-B",
         1: "ASTERIX",
         2: "MLAT",
-        3: "FLARM",
     }
 
-    bbox: Tuple[float, float, float, float] = Property(
+    bbox = Property(
+        (float, float, float, float),
         default=None,
         doc="Bounding box to filter data to (left, bottom, right, top). "
             "Default `None` which will include global data.")
-    timestep: datetime.timedelta = Property(
+    timestep = Property(
+        datetime.timedelta,
         default=datetime.timedelta(seconds=15),
-        doc="Time of each poll after reported time from OpenSky. "
-            "Must be greater than 10 seconds. Default 15 seconds.")
+        doc="Time between each poll. Must be greater than 10 seconds."
+            "Default 15 seconds.")
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self._detections = set()
 
         if self.timestep < datetime.timedelta(seconds=10):
             raise ValueError("'timestep' must be >= 10 seconds.")
 
-    def data_gen(self):
+    @property
+    def detections(self):
+        return self._detections.copy()
+
+    def detections_gen(self):
         if self.bbox:
             params = {
              'lomin': self.bbox[0],
@@ -76,22 +75,22 @@ class _OpenSkyNetworkReader(Reader):
                 response.raise_for_status()
                 data = response.json()
 
-                states_and_metadata = []
+                self._detections = set()
                 for state in data['states']:
                     if state[8]:  # On ground
                         continue
                     # Must have position (lon, lat, geo-alt)
                     if not all(state[index] for index in (5, 6, 13)):
                         continue
-                    timestamp = datetime.datetime.fromtimestamp(
-                        state[3], datetime.timezone.utc).replace(tzinfo=None)
+                    timestamp = datetime.datetime.utcfromtimestamp(state[3])
                     # Skip old detections
                     if time is not None and timestamp <= time:
                         continue
 
-                    states_and_metadata.append((
-                        State([[state[5]], [state[6]], [state[13]]], timestamp=timestamp),
-                        {
+                    self._detections.add(Detection(
+                        StateVector([[state[5]], [state[6]], [state[13]]]),
+                        timestamp=timestamp,
+                        metadata={
                             'icao24': state[0],
                             'callsign': state[1],
                             'orign_country': state[2],
@@ -101,73 +100,8 @@ class _OpenSkyNetworkReader(Reader):
                             'source': self.sources[state[16]],
                         }
                     ))
-                time = datetime.datetime.fromtimestamp(
-                    data['time'], datetime.timezone.utc).replace(tzinfo=None)
-                yield time, states_and_metadata
+                time = datetime.datetime.utcfromtimestamp(data['time'])
+                yield time, self.detections
 
-                while time + self.timestep > datetime.datetime.now(
-                        datetime.timezone.utc).replace(tzinfo=None):
-                    sleep(0.1)
-
-
-class OpenSkyNetworkDetectionReader(_OpenSkyNetworkReader, DetectionReader):
-    """OpenSky Network detection reader
-
-    This reader uses the `OpenSky Network <https://opensky-network.org/>`_ REST
-    API to fetch air traffic control data.
-
-    The detection state vector consists of longitude, latitude
-    (in decimal degrees) and altitude (in meters).
-
-    .. note::
-
-        By using this reader, you are agreeing to `OpenSky Network's terms of
-        use <https://opensky-network.org/about/terms-of-use>`_.
-
-    """
-
-    @BufferedGenerator.generator_method
-    def detections_gen(self):
-        for time, states_and_metadata in self.data_gen():
-            yield time, {Detection(state.state_vector, state.timestamp, metadata)
-                         for state, metadata in states_and_metadata}
-
-
-class OpenSkyNetworkGroundTruthReader(_OpenSkyNetworkReader, GroundTruthReader):
-    """OpenSky Network groundtruth reader
-
-    This reader uses the `OpenSky Network <https://opensky-network.org/>`_ REST
-    API to fetch air traffic control data.
-
-    The groundtruth state vector consists of longitude, latitude
-    (in decimal degrees) and altitude (in meters).
-
-    Paths that are yielded are grouped based on the International Civil Aviation
-    Organisation (ICAO) 24-bit address.
-
-    .. note::
-
-        By using this reader, you are agreeing to `OpenSky Network's terms of
-        use <https://opensky-network.org/about/terms-of-use>`_.
-
-    """
-
-    @BufferedGenerator.generator_method
-    def groundtruth_paths_gen(self):
-        groundtruth_dict = {}
-
-        for time, states_and_metadata in self.data_gen():
-            updated_paths = set()
-            for state, metadata in states_and_metadata:
-                path_id = metadata.get('icao24')
-                if path_id is None:
-                    path = GroundTruthPath()
-                else:
-                    if path_id not in groundtruth_dict:
-                        groundtruth_dict[path_id] = GroundTruthPath([], id=path_id)
-                    path = groundtruth_dict[path_id]
-
-                path.append(GroundTruthState(state.state_vector, state.timestamp, metadata))
-                updated_paths.add(groundtruth_dict[path_id])
-
-            yield time, updated_paths
+                while time + self.timestep > datetime.datetime.utcnow():
+                    sleep(0.5)
