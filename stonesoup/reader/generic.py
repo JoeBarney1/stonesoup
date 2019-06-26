d6e3dfb5a2664882c1b305dd4225c9a7e13017b0
declan <dcrew@dstl.gov.uk>
2019-06-26 09:46:03 +0100

    Make timestamp read scientific notation formatted ints

--- stonesoup/reader/generic.py
+++ stonesoup/reader/generic.py
@@ -147,7 +147,7 @@ class CSVDetectionReader(DetectionReader, _CSVReader):
                         row[self.time_field], self.time_field_format)
                 elif self.timestamp is True:
                     time_field_value = datetime.utcfromtimestamp(
-                        int(row[self.time_field]))
+                        int(float(row[self.time_field])))
                 else:
                     time_field_value = parse(row[self.time_field])
 
