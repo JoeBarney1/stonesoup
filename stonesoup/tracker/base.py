1977bf76cb223dfb2d206e854e5a9fc67e7afa36
declan <dcrew@dstl.gov.uk>
2019-06-27 13:26:02 +0100

    Update trackers to use BufferedGenerator

--- stonesoup/tracker/base.py
+++ stonesoup/tracker/base.py
@@ -3,23 +3,11 @@ from abc import abstractmethod
 from typing import Iterator, Set, Tuple
 
 from ..base import Base
-from ..types.detection import Detection
-from ..types.track import Track
+from stonesoup.buffered_generator import BufferedGenerator
 
 
-class Tracker(Base):
+class Tracker(Base, BufferedGenerator):
     """Tracker base class"""
-
-    @property
-    @abstractmethod
-    def tracks(self):
-        """The tracks at the current time step.
-
-        This is the set of tracks last returned by the
-        :meth:`tracks_gen` generator, to allow other components, like
-        metrics, to access the data.
-        """
-        raise NotImplementedError
 
     @abstractmethod
     def tracks(self) -> Set[Track]:
