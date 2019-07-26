f4eb7688065552ef9a8932e0182c8c3a06a41eed
lflaherty-dstl <lflaherty@dstl.gov.uk>
2019-07-26 09:11:35 +0100

    Add 2d assignment alg + new gnn

--- stonesoup/dataassociator/neighbour.py
+++ stonesoup/dataassociator/neighbour.py
@@ -1,14 +1,12 @@
-import itertools
-
+# -*- coding: utf-8 -*-
 import numpy as np
-from scipy.optimize import linear_sum_assignment
 
 from .base import DataAssociator
 from ..base import Property
 from ..hypothesiser import Hypothesiser
-from ..types.hypothesis import SingleHypothesis, JointHypothesis, \
-    ProbabilityHypothesis
-
+from ..assignment import assignAlgs
+from ..types.detection import MissedDetection
+from ..types.hypothesis import SingleDistanceHypothesis
 
 class NearestNeighbour(DataAssociator):
     """Nearest Neighbour Associator
@@ -171,81 +169,77 @@ class GNNWith2DAssignment(DataAssociator):
         # Generate a set of hypotheses for each track on each detection
         hypotheses = self.generate_hypotheses(tracks, detections, timestamp, **kwargs)
 
-        # Create dictionary for associations
-        associations = {}
+        # Link hypotheses into a set of joint_hypotheses and evaluate
+        joint_hypotheses = self.enumerate_joint_hypotheses(hypotheses)
+        associations = max(joint_hypotheses)
+        
+        return associations
 
-        # Extract detected tracks
-        detected_tracks = [track
-                           for track, track_hypotheses in hypotheses.items()
-                           if any(track_hypotheses)]
+class GNNwith2DAssignment(DataAssociator):
+    """Global Nearest Neighbour Associator
 
-        # Store associations for undetected/missed tracks
-        # NOTE: It is assumed that if a track is undetected/missed, then it
-        #       will only have a single missed detection hypothesis
-        for track in hypotheses.keys() - set(detected_tracks):
-            if hypotheses[track]:
-                associations[track] = hypotheses[track][0]
+    Associates detections to a predicted state using the Global Nearest Neighbour method, 
+    utilising a 2D matrix of distances and a "shortest path" assignment algorithm.
+    """
 
-        # No need to perform data association if all tracks are missed
-        if not detected_tracks:
-            return associations
+    hypothesiser = Property(
+        Hypothesiser,
+        doc="Generate a set of hypotheses for each prediction-detection pair")
 
+    def associate(self, tracks, detections, time):
+        """Associate a set of detections with predicted states.
+
+        Parameters
+        ----------
+        tracks : set of :class:`Track`
+            Current tracked objects
+        detections : set of :class:`Detection`
+            Retrieved measurements
+        time : datetime
+            Detection time to predict to
+
+        Returns
+        -------
+        dict
+            Key value pair of tracks with associated detection
+        """
+        
         # Convert sets to indexable lists
+        tracks = list(tracks)
         detections = list(detections)
-
+        
         # Generate 2d array "matrix" of hypotheses mapping track to detection
-        hypothesis_matrix = np.empty(
-            (len(detected_tracks), len(detections) + len(detected_tracks)),
-            SingleHypothesis)
-        for i, track in enumerate(detected_tracks):
-            row = np.empty(
-                (hypothesis_matrix.shape[1]), SingleHypothesis)
-            for hypothesis in hypotheses[track]:
-                if not hypothesis:
+        hypothesis_matrix = np.empty([len(tracks), len(detections) + len(tracks)], SingleDistanceHypothesis)
+        for i in range(len(tracks)):
+            row = np.empty([len(detections) + len(tracks)], SingleDistanceHypothesis)
+            track_hypotheses = self.hypothesiser.hypothesise(tracks[i], detections, time)
+            for hypothesis in track_hypotheses:
+                if isinstance(hypothesis.measurement, MissedDetection):
                     row[len(detections) + i] = hypothesis
                 else:
-                    row[detections.index(hypothesis.measurement)] = hypothesis
+                    row[int(detections.index(hypothesis.measurement))] = hypothesis
             hypothesis_matrix[i] = row
-
-        # Determine type of hypothesis used, probability or distance
-        # Probability is maximise problem, distance is minimise problem
-        # Mixed hypotheses cannot be computed at this time
-        hypothesis_types = {
-            isinstance(hypothesis, ProbabilityHypothesis)
-            for row in hypothesis_matrix for hypothesis in row
-            if hypothesis is not None}
-        if len(hypothesis_types) > 1:
-            raise RuntimeError(
-                "2d assignment does not support mixed hypothesis types")
-        probability_flag = hypothesis_types.pop()
-
+        
         # Generate 2d array "matrix" of distances
-        # Use probabilities instead for probability based hypotheses
-        distance_matrix = np.empty(hypothesis_matrix.shape)
+        distance_matrix = np.empty([len(tracks), len(detections) + len(tracks)])
         for x in range(hypothesis_matrix.shape[0]):
             for y in range(hypothesis_matrix.shape[1]):
                 if hypothesis_matrix[x][y] is None:
-                    distance_matrix[x][y] = -np.inf if probability_flag \
-                        else np.inf
+                    distance_matrix[x][y] = np.inf
                 else:
-                    if probability_flag:
-                        distance_matrix[x][y] = \
-                            hypothesis_matrix[x][y].probability
-                    else:
-                        distance_matrix[x][y] = \
-                            hypothesis_matrix[x][y].distance
-
-        # Use "shortest path" assignment algorithm on distance matrix
-        # to assign tracks to nearest detection
-        # Maximise flag = true for probability instance
-        # (converts minimisation problem to maximisation problem)
-        try:
-            row4col, col4row = linear_sum_assignment(distance_matrix, probability_flag)
-        except ValueError:
-            raise RuntimeError("Assignment was not feasible")
-
+                    distance_matrix[x][y] = hypothesis_matrix[x][y].distance
+        
+        # Use "shortest path" assignment algorithm on distance matrix to assign tracks to nearest detection
+        gain, col4row, row4col = assignAlgs.assign2D(distance_matrix)
+        
+        # Ensure the problem was feasible
+        if gain == np.empty(0):
+            print("IMPOSSIBLE ASSIGNMENT")
+            return
+        
         # Generate dict of key/value pairs
-        for j, track in enumerate(detected_tracks):
-            associations[track] = hypothesis_matrix[j][col4row[j]]
-
+        associations = {}
+        for j in range(len(col4row)):
+            associations[tracks[j]] = hypothesis_matrix[j][int(col4row[j])]
+        
         return associations
