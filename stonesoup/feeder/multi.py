db1668cfc063bc534ba5e3e4ce6cdd92bcc2eb6d
Steven Hiscocks <sdhiscocks@dstl.gov.uk>
2019-11-25 16:43:05 +0000

    Add Multi-detector Feeder class

--- stonesoup/feeder/multi.py
+++ stonesoup/feeder/multi.py
@@ -1,21 +1,40 @@
-import heapq
-from typing import Collection
-
-from .base import DetectionFeeder, GroundTruthFeeder
+# -*- coding: utf-8 -*-
+from .base import Feeder
 from ..base import Property
 from ..buffered_generator import BufferedGenerator
-from ..reader import Reader
+from ..detector import Detector
 
 
-class MultiDataFeeder(DetectionFeeder, GroundTruthFeeder):
-    """Multi-data Feeder
+class MultiDetectorFeeder(Feeder):
+    """Multi-detector Feeder
 
-    This returns states from multiple data readers as a single stream,
-    yielding from the reader yielding the lowest timestamp first.
+    This returns detections from multiple detectors as a single stream,
+    yielding from the detector yielding the lowest timestamp first.
     """
-    reader = None
-    readers: Collection[Reader] = Property(doc='Readers to yield from')
+    detector = None
+    detectors = Property([Detector], doc="Detectors to yield from")
 
     @BufferedGenerator.generator_method
-    def data_gen(self):
-        yield from heapq.merge(*self.readers)
+    def detections_gen(self):
+        detector_iters = (iter(detector) for detector in self.detectors)
+        iter_detections = {
+            detector_iter: next(detector_iter)
+            for detector_iter in detector_iters}
+
+        min_time = None
+        while iter_detections:  # Whilst still iterators left
+            for detector_iter, (time, detections) in iter_detections.items():
+                if min_time is None or time < min_time:
+                    min_time = time
+                    min_detector_iter = detector_iter
+                    min_detections = detections
+
+            yield min_time, min_detections
+            min_time = None
+
+            try:
+                # Grab next set for this iter
+                iter_detections[min_detector_iter] = next(min_detector_iter)
+            except StopIteration:
+                # Empty iterator, remove from dictionary
+                del iter_detections[min_detector_iter]
--- stonesoup/feeder/tests/test_multi.py
+++ stonesoup/feeder/tests/test_multi.py
@@ -1,15 +1,15 @@
+# -*- coding: utf-8 -*-
 import pytest
 
-from ..multi import MultiDataFeeder
+from ..multi import MultiDetectorFeeder
 
 
 @pytest.mark.parametrize('n', [1, 2, 3])
-def test_multi_detections(n, reader):
+def test_multi(n, detector):
     single_time_list = list()
     multi_time_list = list()
-
-    multi_detector = MultiDataFeeder([reader]*n)
-    for single_iterations, (time, _) in enumerate(reader, 1):
+    multi_detector = MultiDetectorFeeder([detector]*n)
+    for single_iterations, (time, _) in enumerate(detector, 1):
         single_time_list.append(time)
     for multi_iterations, (time, _) in enumerate(multi_detector, 1):
         multi_time_list.append(time)
