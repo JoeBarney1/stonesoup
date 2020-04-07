f7da5a3377d5785e2893e01efea9ba40598b1047
Steven Hiscocks <sdhiscocks@dstl.gov.uk>
2020-04-07 11:46:31 +0100

    Modify detection and ground truth CSV readers
    - Modifications try to reuse code more now where possible
    - Groundtruth now includes id
    - Groundtruth and detctions that of the same timestamp are yielded together

--- stonesoup/reader/generic.py
+++ stonesoup/reader/generic.py
@@ -5,22 +5,22 @@ of data that is in common formats.
 """
 
 import csv
-from collections import defaultdict
 from datetime import datetime, timedelta
 from math import modf
 
 import numpy as np
 from dateutil.parser import parse
 
-from ..base import Property
-from ..types.detection import Detection
 from .base import GroundTruthReader, DetectionReader
 from .file import TextFileReader
+from ..base import Property
+from ..buffered_generator import BufferedGenerator
+from ..types.detection import Detection
 from ..types.groundtruth import GroundTruthPath, GroundTruthState
 from stonesoup.buffered_generator import BufferedGenerator
 
 
-class CSVGroundTruthReader(GroundTruthReader, TextFileReader):
+class _CSVReader(TextFileReader):
     state_vector_fields = Property(
         [str], doc='List of columns names to be used in state vector')
     time_field = Property(
@@ -29,61 +29,10 @@ class CSVGroundTruthReader(GroundTruthReader, TextFileReader):
         str, default=None, doc='Optional datetime format')
     timestamp = Property(
         bool, default=False, doc='Treat time field as a timestamp from epoch')
-    path_id_field = Property(
-        str, doc='Name of column to be used as path ID')
+    metadata_fields = Property(
+        [str], default=None, doc='List of columns to be saved as metadata, default all')
     csv_options = Property(
-        dict, default={},
-        doc='Keyword arguments for the underlying csv reader')
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._groundtruth_paths = set()
-
-    @property
-    def groundtruth_paths(self):
-        return self._groundtruth_paths.copy()
-
-    @BufferedGenerator.generator_method
-    def groundtruth_paths_gen(self):
-        with self.path.open(encoding=self.encoding, newline='') as csv_file:
-            groundtruth_dict = defaultdict(GroundTruthPath)
-
-            reader = csv.DictReader(csv_file, **self.csv_options)
-            for row in reader:
-                if self.time_field_format is not None:
-                    time_field_value = datetime.strptime(
-                        row[self.time_field], self.time_field_format)
-                elif self.timestamp is True:
-                    fractional, timestamp = modf(float(row[self.time_field]))
-                    time_field_value = datetime.utcfromtimestamp(
-                        int(timestamp))
-                    time_field_value += timedelta(microseconds=fractional*1E6)
-                else:
-                    time_field_value = parse(row[self.time_field])
-
-                state = GroundTruthState(np.array(
-                    [[row[col_name]] for col_name in self.state_vector_fields],
-                    dtype=np.float32), time_field_value)
-
-                groundtruth_dict[row[self.path_id_field]].append(state)
-                self._groundtruth_paths = set(groundtruth_dict.values())
-
-                yield time_field_value, self._groundtruth_paths
-
-
-class _CSVReader(TextFileReader):
-    state_vector_fields: Sequence[str] = Property(
-        doc='List of columns names to be used in state vector')
-    time_field: str = Property(
-        doc='Name of column to be used as time field')
-    time_field_format: str = Property(
-        default=None, doc='Optional datetime format')
-    timestamp: bool = Property(
-        default=False, doc='Treat time field as a timestamp from epoch')
-    metadata_fields: Collection[str] = Property(
-        default=None, doc='List of columns to be saved as metadata, default all')
-    csv_options: Mapping = Property(
-        default={}, doc='Keyword arguments for the underlying csv reader')
+        dict, default={}, doc='Keyword arguments for the underlying csv reader')
 
     def _get_metadata(self, row):
         if self.metadata_fields is None:
@@ -102,8 +51,7 @@ class _CSVReader(TextFileReader):
             time_field_value = datetime.strptime(row[self.time_field], self.time_field_format)
         elif self.timestamp is True:
             fractional, timestamp = modf(float(row[self.time_field]))
-            time_field_value = datetime.fromtimestamp(
-                int(timestamp), timezone.utc).replace(tzinfo=None)
+            time_field_value = datetime.utcfromtimestamp(int(timestamp))
             time_field_value += timedelta(microseconds=fractional * 1E6)
         else:
             time_field_value = parse(row[self.time_field], ignoretz=True)
@@ -121,7 +69,8 @@ class CSVGroundTruthReader(GroundTruthReader, _CSVReader):
     Parameters
     ----------
     """
-    path_id_field: str = Property(doc='Name of column to be used as path ID')
+    path_id_field = Property(
+        str, doc='Name of column to be used as path ID')
 
     @BufferedGenerator.generator_method
     def groundtruth_paths_gen(self):
@@ -139,8 +88,8 @@ class CSVGroundTruthReader(GroundTruthReader, _CSVReader):
 
                 state = GroundTruthState(
                     np.array([[row[col_name]] for col_name in self.state_vector_fields],
-                             dtype=np.float64),
-                    timestamp=time,
+                             dtype=np.float_),
+                    timestamp=self._get_time(row),
                     metadata=self._get_metadata(row))
 
                 id_ = row[self.path_id_field]
@@ -165,51 +114,24 @@ class CSVDetectionReader(DetectionReader, _CSVReader):
     ----------
     """
 
-    state_vector_fields = Property(
-        [str], doc='List of columns names to be used in state vector')
-    time_field = Property(
-        str, doc='Name of column to be used as time field')
-    time_field_format = Property(
-        str, default=None, doc='Optional datetime format')
-    timestamp = Property(
-        bool, default=False, doc='Treat time field as a timestamp from epoch')
-    metadata_fields = Property(
-        [str], default=None, doc='List of columns to be saved as metadata, '
-                                 'default all')
-    csv_options = Property(
-        dict, default={},
-        doc='Keyword arguments for the underlying csv reader')
-
     @BufferedGenerator.generator_method
     def detections_gen(self):
         with self.path.open(encoding=self.encoding, newline='') as csv_file:
-            reader = csv.DictReader(csv_file, **self.csv_options)
-            for row in reader:
-                if self.time_field_format is not None:
-                    time_field_value = datetime.strptime(
-                        row[self.time_field], self.time_field_format)
-                elif self.timestamp is True:
-                    fractional, timestamp = modf(float(row[self.time_field]))
-                    time_field_value = datetime.utcfromtimestamp(
-                        int(timestamp))
-                    time_field_value += timedelta(microseconds=fractional*1E6)
-                else:
-                    time_field_value = parse(row[self.time_field])
+            detections = set()
+            previous_time = None
+            for row in csv.DictReader(csv_file, **self.csv_options):
 
-                if self.metadata_fields is None:
-                    local_metadata = dict(row)
-                    copy_local_metadata = dict(local_metadata)
-                    for (key, value) in copy_local_metadata.items():
-                        if (key == self.time_field) or \
-                                (key in self.state_vector_fields):
-                            del local_metadata[key]
-                else:
-                    local_metadata = {field: row[field]
-                                      for field in self.metadata_fields
-                                      if field in row}
+                time = self._get_time(row)
+                if previous_time is not None and previous_time != time:
+                    yield previous_time, detections
+                    detections = set()
+                previous_time = time
 
-                detect = Detection(np.array(
-                    [[row[col_name]] for col_name in self.state_vector_fields],
-                    dtype=np.float32), time_field_value,
-                    metadata=local_metadata)
-                yield time_field_value, {detect}
+                detections.add(Detection(
+                    np.array([[row[col_name]] for col_name in self.state_vector_fields],
+                             dtype=np.float_),
+                    timestamp=time,
+                    metadata=self._get_metadata(row)))
+
+            # Yield remaining
+            yield previous_time, detections
--- stonesoup/reader/tests/test_generic.py
+++ stonesoup/reader/tests/test_generic.py
@@ -8,7 +8,8 @@ import pytest
 from ..generic import CSVDetectionReader, CSVGroundTruthReader
 
 
-def test_csv_gt(tmpdir):
+@pytest.fixture()
+def csv_gt_filename(tmpdir):
     csv_filename = tmpdir.join("test.csv")
     with csv_filename.open('w') as csv_file:
         csv_file.write(dedent("""\
@@ -19,24 +20,22 @@ def test_csv_gt(tmpdir):
                 13,23,33,32018332,2018-01-01T14:03:00Z
                 14,24,34,32018332,2018-01-01T14:04:00Z
                 """))
+    return csv_filename
 
+
+def test_csv_gt_2d(csv_gt_filename):
     # run test with:
     #   - 2d co-ordinates
     #   - default time field format
     #   - no special csv options
-    csv_reader = CSVGroundTruthReader(csv_filename.strpath,
+    csv_reader = CSVGroundTruthReader(csv_gt_filename.strpath,
                                       state_vector_fields=["x", "y"],
                                       time_field="t",
                                       path_id_field="identifier")
 
-    all_gt_paths = [
-        gt_paths_at_timestep
-        for timestep, gt_paths_at_timestep
-        in csv_reader.groundtruth_paths_gen()]
-
-    final_gt_paths = [
-        gt_path
-        for gt_path in all_gt_paths[len(all_gt_paths) - 1]]
+    final_gt_paths = set()
+    for _, gt_paths_at_timestep in csv_reader:
+        final_gt_paths.update(gt_paths_at_timestep)
     assert len(final_gt_paths) == 2
 
     ground_truth_states = [
@@ -52,23 +51,20 @@ def test_csv_gt(tmpdir):
         assert gt_state.timestamp.minute == n
         assert gt_state.timestamp.date() == datetime.date(2018, 1, 1)
 
+
+def test_csv_gt_3d_time(csv_gt_filename):
     # run test with:
     #   - 3d co-ordinates
     #   - time field format specified
-    csv_reader = CSVGroundTruthReader(csv_filename.strpath,
+    csv_reader = CSVGroundTruthReader(csv_gt_filename.strpath,
                                       state_vector_fields=["x", "y", "z"],
                                       time_field="t",
                                       time_field_format="%Y-%m-%dT%H:%M:%SZ",
                                       path_id_field="identifier")
 
-    all_gt_paths = [
-        gt_paths_at_timestep
-        for timestep, gt_paths_at_timestep
-        in csv_reader.groundtruth_paths_gen()]
-
-    final_gt_paths = [
-        gt_path
-        for gt_path in all_gt_paths[len(all_gt_paths) - 1]]
+    final_gt_paths = set()
+    for _, gt_paths_at_timestep in csv_reader:
+        final_gt_paths.update(gt_paths_at_timestep)
     assert len(final_gt_paths) == 2
 
     ground_truth_states = [
@@ -84,10 +80,13 @@ def test_csv_gt(tmpdir):
         assert gt_state.timestamp.minute == n
         assert gt_state.timestamp.date() == datetime.date(2018, 1, 1)
 
+
+def test_csv_gt_3d_timestamp_csv_opt(tmpdir):
     # run test with:
     #   - time field represented as a Unix epoch timestamp
     #   - csv options specified
-    with csv_filename.open('w') as csv_file:
+    csv_gt_filename = tmpdir.join("test.csv")
+    with csv_gt_filename.open('w') as csv_file:
         csv_file.write(dedent("""\
                 10,20,30,22018332,1514815200
                 11,21,31,22018332,1514815260
@@ -96,7 +95,7 @@ def test_csv_gt(tmpdir):
                 14,24,34,32018332,1514815440
                 """))
 
-    csv_reader = CSVGroundTruthReader(csv_filename.strpath,
+    csv_reader = CSVGroundTruthReader(csv_gt_filename.strpath,
                                       state_vector_fields=["x", "y", "z"],
                                       time_field="t",
                                       timestamp=True,
@@ -105,14 +104,9 @@ def test_csv_gt(tmpdir):
                                                     ['x', 'y', 'z',
                                                         'identifier', 't']})
 
-    all_gt_paths = [
-        gt_paths_at_timestep
-        for timestep, gt_paths_at_timestep
-        in csv_reader.groundtruth_paths_gen()]
-
-    final_gt_paths = [
-        gt_path
-        for gt_path in all_gt_paths[len(all_gt_paths) - 1]]
+    final_gt_paths = set()
+    for _, gt_paths_at_timestep in csv_reader:
+        final_gt_paths.update(gt_paths_at_timestep)
     assert len(final_gt_paths) == 2
 
     ground_truth_states = [
@@ -129,8 +123,32 @@ def test_csv_gt(tmpdir):
         assert gt_state.timestamp.date() == datetime.date(2018, 1, 1)
 
 
+def test_csv_gt_multi_per_timestep(tmpdir):
+    csv_gt_filename = tmpdir.join("test.csv")
+    with csv_gt_filename.open('w') as csv_file:
+        csv_file.write(dedent("""\
+                x,y,z,identifier,t
+                10,20,30,22018332,2018-01-01T14:00:00Z
+                11,21,31,22018332,2018-01-01T14:01:00Z
+                12,22,32,22018332,2018-01-01T14:02:00Z
+                13,23,33,32018332,2018-01-01T14:02:00Z
+                14,24,34,32018332,2018-01-01T14:03:00Z
+                """))
+
+    csv_reader = CSVGroundTruthReader(csv_gt_filename.strpath,
+                                      state_vector_fields=["x", "y"],
+                                      time_field="t",
+                                      path_id_field="identifier")
+
+    for time, ground_truth_paths in csv_reader:
+        if time == datetime.datetime(2018, 1, 1, 14, 2):
+            assert len(ground_truth_paths) == 2
+        else:
+            assert len(ground_truth_paths) == 1
+
+
 @pytest.fixture()
-def csv_gt_filename(tmpdir):
+def csv_det_filename(tmpdir):
     csv_filename = tmpdir.join("test.csv")
     with csv_filename.open('w') as csv_file:
         csv_file.write(dedent("""\
@@ -139,11 +157,14 @@ def csv_gt_filename(tmpdir):
             11,21,31,22018332,2018-01-01T14:01:00Z
             12,22,32,22018332,2018-01-01T14:02:00Z
             """))
+    return csv_filename
 
+
+def test_csv_default(csv_det_filename):
     # run test with:
     #   - 'metadata_fields' for 'CSVDetectionReader' == default
     #   - copy all metadata items
-    csv_reader = CSVDetectionReader(csv_filename.strpath, ["x", "y"], "t")
+    csv_reader = CSVDetectionReader(csv_det_filename.strpath, ["x", "y"], "t")
     detections = [
         detection
         for _, detections in csv_reader
@@ -162,11 +183,13 @@ def csv_gt_filename(tmpdir):
         assert int(detection.metadata['z']) == 30 + n
         assert detection.metadata['identifier'] == '22018332'
 
+
+def test_csv_metadata_time(csv_det_filename):
     # run test with:
     #   - 'metadata_fields' for 'CSVDetectionReader' contains
     #       'z' but not 'identifier'
     #   - 'time_field_format' is specified
-    csv_reader = CSVDetectionReader(csv_filename.strpath, ["x", "y"], "t",
+    csv_reader = CSVDetectionReader(csv_det_filename.strpath, ["x", "y"], "t",
                                     time_field_format="%Y-%m-%dT%H:%M:%SZ",
                                     metadata_fields=["z"])
     detections = [
@@ -185,11 +208,14 @@ def csv_gt_filename(tmpdir):
         assert 'z' in detection.metadata.keys()
         assert int(detection.metadata['z']) == 30 + n
 
+
+def test_csv_missing_metadata_timestamp(tmpdir):
     # run test with:
     #   - 'metadata_fields' for 'CSVDetectionReader' contains
     #       column names that do not exist in CSV file
     #   - 'time' field represented as a Unix epoch timestamp
-    with csv_filename.open('w') as csv_file:
+    csv_det_filename = tmpdir.join("test.csv")
+    with csv_det_filename.open('w') as csv_file:
         csv_file.write(dedent("""\
             x,y,z,identifier,t
             10,20,30,22018332,1514815200
@@ -197,7 +223,7 @@ def csv_gt_filename(tmpdir):
             12,22,32,22018332,1514815320
             """))
 
-    csv_reader = CSVDetectionReader(csv_filename.strpath, ["x", "y"], "t",
+    csv_reader = CSVDetectionReader(csv_det_filename.strpath, ["x", "y"], "t",
                                     metadata_fields=["heading"],
                                     timestamp=True)
     detections = [
@@ -213,6 +239,27 @@ def csv_gt_filename(tmpdir):
         assert detection.timestamp.date() == datetime.date(2018, 1, 1)
         assert isinstance(detection.metadata, dict)
         assert len(detection.metadata) == 0
+
+
+def test_csv_multi_per_timestep(tmpdir):
+    csv_det_filename = tmpdir.join("test.csv")
+    with csv_det_filename.open('w') as csv_file:
+        csv_file.write(dedent("""\
+                x,y,z,identifier,t
+                10,20,30,22018332,2018-01-01T14:00:00Z
+                11,21,31,22018332,2018-01-01T14:01:00Z
+                12,22,32,22018332,2018-01-01T14:02:00Z
+                13,23,33,32018332,2018-01-01T14:02:00Z
+                14,24,34,32018332,2018-01-01T14:03:00Z
+                """))
+
+    csv_reader = CSVDetectionReader(csv_det_filename.strpath, ["x", "y"], "t")
+
+    for time, detections in csv_reader:
+        if time == datetime.datetime(2018, 1, 1, 14, 2):
+            assert len(detections) == 2
+        else:
+            assert len(detections) == 1
 
 
 def test_tsv(tmpdir):
