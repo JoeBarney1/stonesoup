934ed51a31be0d687512be3fbfac07d8d3e96437
Steven Hiscocks <sdhiscocks@dstl.gov.uk>
2020-04-24 17:21:51 +0100

    Add StateVectors type and modify array handling
    This includes a number of changes to how Matrix type is implemented
    which will allow greater customisation if required. In particular, a new
    StateVectors type has been created which handles the case where
    calculations of averages (and in turn covariance) were incorrect when
    using Angle types.
    
    Angle types now include a average method, which returns a circular mean.
    This removes an issue when calculating means near the point of angle
    wrapping.
    
    These changes were primarily done for bug identified when calculating
    mean of particles, and such some minor changes to PartcileState and
    Particle type have been made.
    
    Some functions have also been updated, as these are a set of state
    vectors, so can take advantage of the new type. This includes updating
    the Mixture Tracker
    
    The array changes have changed minimum requirement for NumPy to 1.17, or
    1.16 with NUMPY_EXPERIMENTAL_ARRAY_FUNCTION environment variable set to
    1.

--- stonesoup/models/base.py
+++ stonesoup/models/base.py
@@ -10,7 +10,7 @@ from scipy.stats import multivariate_normal
 
 from ..base import Base
 from ..functions import jacobian as compute_jac
-from ..types.array import Matrix, StateVector
+from ..types.array import StateVector, StateVectors
 from ..types.numeric import Probability
 
 
@@ -325,7 +325,7 @@ class GaussianModel(Model):
         if num_samples == 1:
             return noise.view(StateVector)
         else:
-            return noise.view(Matrix)
+            return noise.view(StateVectors)
 
     def pdf(self, state1, state2, **kwargs):
         r"""Model pdf/likelihood evaluation function
