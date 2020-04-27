1906dbd5f3288eec01869bd766271dddb02a09ae
Steven Hiscocks <sdhiscocks@dstl.gov.uk>
2020-04-27 08:52:58 +0100

    Fix issue with logpdf not handling custom types (e.g. Angle)

--- stonesoup/models/base.py
+++ stonesoup/models/base.py
@@ -353,9 +353,10 @@ class GaussianModel(Model):
             The likelihood of ``state1``, given ``state2``
         """
 
+        # Calculate difference before to handle custom types (mean defaults to zero)
+        # This is required as log pdf coverts arrays to floats
         likelihood = multivariate_normal.logpdf(
-            state_vector1.T,
-            mean=self.function(state_vector2, **kwargs).ravel(),
+            (state1.state_vector - self.function(state2, **kwargs)).ravel(),
             cov=self.covar(**kwargs)
         )
         return Probability(likelihood, log_value=True)
