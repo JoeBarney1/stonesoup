fc0202d6b60cf555d0624dc41740aa7af88ffc73
Richard Green <38097873+rjgreen-dstl@users.noreply.github.com>
2020-05-13 11:18:51 +0100

    Update simple.py
    modified datetime to datetime.datetime

--- stonesoup/simulator/simple.py
+++ stonesoup/simulator/simple.py
@@ -280,7 +280,7 @@ class DummyGroundTruthSimulator(GroundTruthSimulator):
      It returns an empty set at each time step.
     """
 
-    times = Property([datetime], doc='list to times to return')
+    times = Property([datetime.datetime], doc='list to times to return')
 
     @BufferedGenerator.generator_method
     def groundtruth_paths_gen(self):
