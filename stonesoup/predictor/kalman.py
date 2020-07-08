3d544baf04acb046d50dbf34dc2c34975bf4640c
Steven Hiscocks <sdhiscocks@dstl.gov.uk>
2020-07-08 10:47:05 +0100

    Fix caching issue with states/mutable-sequences with predictor
    When passing a `StateMutableSequence` (e.g. a track) to a predictor
    using caching, the cache can incorrectly return results if same track
    and time stamp are passed in multiple times. This can be the case with
    multi-sensor example, where an update has occurred from one sensor, then
    a detection from another sensor is processed at the same time stamp.
    
    This fixes the issue by using a custom lru_cache decorator that extracts
    the current state if input is mutable sequence.

--- stonesoup/predictor/kalman.py
+++ stonesoup/predictor/kalman.py
@@ -1,10 +1,11 @@
 from functools import partial
 
 import numpy as np
-from functools import lru_cache, partial
+from functools import partial
 
-from ..base import Property
 from .base import Predictor
+from ._utils import predict_lru_cache
+from ..base import Property
 from ..types.prediction import GaussianStatePrediction
 from ..models.base import LinearModel
 from ..models.transition import TransitionModel
@@ -112,7 +113,7 @@ class KalmanPredictor(Predictor):
 
         return predict_over_interval
 
-    @lru_cache()
+    @predict_lru_cache()
     def predict(self, prior, timestamp=None, **kwargs):
         r"""The predict function
         Parameters
@@ -291,7 +292,7 @@ class UnscentedKalmanPredictor(KalmanPredictor):
                 prior_state_vector, **kwargs) \
             + self.control_model.control_input()
 
-    @lru_cache()
+    @predict_lru_cache()
     def predict(self, prior, timestamp=None, **kwargs):
         r"""The unscented version of the predict step
         Parameters
