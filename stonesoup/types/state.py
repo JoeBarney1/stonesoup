083cf36147246bc1b1ac8b66aa6b7bde97ddc245
jmbarr <31851985+jmbarr@users.noreply.github.com>
2020-08-07 08:33:49 +0100

    Sqrt kalma filter (#223)
    * Creation of a square root Gaussian state type.
    
    * SqrtKalmanPredictor (and tests) added
    
    * SqrtKalmanUpdater (and tests) added.
    
    * Swapped additional variables in update() for instances of Property in KalmanUpdater
    
    * Adjusted to accommodate classes which use sqrt_covar attributes rather than carry sqrt matrices in the covar attribute.
    
    Co-authored-by: paulhorridge <P.Horridge@liverpool.ac.uk>

--- stonesoup/types/state.py
+++ stonesoup/types/state.py
@@ -143,6 +143,41 @@ class GaussianState(State):
         return self.state_vector
 
 
+class SqrtGaussianState(State):
+    """A Gaussian State type where the covariance matrix is stored in a form :math:`W` such that
+    :math:`P = WW^T`
+
+    For :math:`P` in general, :math:`W` is not unique and the user may choose the form to their
+    taste. No checks are undertaken to ensure that a sensible square root form has been chosen.
+
+    """
+    sqrt_covar = Property(CovarianceMatrix, doc="A square root form of the Gaussian covariance "
+                                                "matrix.")
+
+    def __init__(self, state_vector, sqrt_covar, *args, **kwargs):
+        sqrt_covar = CovarianceMatrix(sqrt_covar)
+        super().__init__(state_vector, sqrt_covar, *args, **kwargs)
+
+    @property
+    def mean(self):
+        """The state mean, equivalent to state vector"""
+        return self.state_vector
+
+    @property
+    def covar(self):
+        """The full covariance matrix.
+
+        Returns
+        -------
+        : :class:`~.CovarianceMatrix`
+            The covariance matrix calculated via :math:`W W^T`, where :math:`W` is a
+            :class:`~.SqrtCovarianceMatrix`
+
+        """
+        return self.sqrt_covar @ self.sqrt_covar.T
+GaussianState.register(SqrtGaussianState)  # noqa: E305
+
+
 class WeightedGaussianState(GaussianState):
     """Weighted Gaussian State Type
 
