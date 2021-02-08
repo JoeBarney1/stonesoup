9b1066ea8f36d692299012df4bfbd8700a884608
jordi <jmbarr@dstl.gov.uk>
2021-02-08 21:10:09 +0000

    Created Kalman Smoother

--- stonesoup/smoother/kalman.py
+++ stonesoup/smoother/kalman.py
@@ -1,61 +1,26 @@
-import copy
-from functools import partial
-
+# -*- coding: utf-8 -*-
 import numpy as np
 
 from .base import Smoother
 from ..base import Property
-from ..models.base import LinearModel
-from ..types.multihypothesis import MultipleHypothesis
-from ..types.prediction import GaussianStatePrediction
-from ..types.update import GaussianStateUpdate
+from ..types.track import Track
+from ..types.prediction import Prediction, GaussianStatePrediction
+from ..types.update import Update, GaussianStateUpdate
 from ..models.transition.base import TransitionModel
 from ..models.transition.linear import LinearGaussianTransitionModel
-from ..functions import gauss2sigma, unscented_transform
 
 
 class KalmanSmoother(Smoother):
-    r"""
-    The linear-Gaussian or Rauch-Tung-Striebel smoother, colloquially the Kalman smoother [1]_. The
-    transition model is therefore linear-Gaussian. No control model is currently implemented.
+    """
+    The linear-Gaussian or Rauch-Tung-Striebel smoother, colloquially the Kalman smoother.
 
-    TODO: Include a control model
+    The transition model is linear Gaussian
 
-    The smooth function undertakes the backward algorithm on a :class:`~.Track()` object. This is
-    done by starting at the final index in the track, :math:`K` and proceeding from
-    :math:`K \rightarrow 1` via:
-
-    .. math::
-
-        \mathbf{x}_{k|k-1} &= F_{k} \mathbf{x}_{k-1}
-
-        P_{k|k-1} &= F_{k} P_{k-1} F_{k}^T + Q_{k}
-
-        G_k &= P_{k-1} F_{k}^T P_{k|k-1}^{-1}
-
-        \mathbf{x}_{k-1}^s &= \mathbf{x}_{k-1} + G_k (\mathbf{x}_{k}^s - \mathbf{x}_{k|k-1})
-
-        P_{k-1}^s &= P_{k-1} + G_k (P_{k}^s - P_{k|k-1}) G_k^T
-
-    where :math:`\mathbf{x}_{K}^s = \mathbf{x}_{K}` and :math:`P_K^s = P_K`.
-
-    The predicted state vector and covariance are retrieved from the Track via predicted state or
-    updated state via the links therein. Note that this means that the first two equations are not
-    calculated, the results merely retrieved. This smoother is therefore strictly Kalman only in
-    the backward portion. The prediction might have come by any number of means. If present, the
-    transition model (providing :math:`F` and :math:`Q`) in the prediction is used. This allows for
-    a dynamic transition model (i.e. one that changes with :math:`k`). Otherwise, the (static)
-    transition model is used, defined on smoother initialisation.
-
-    References
-
-    .. [1] Särkä S. 2013, Bayesian filtering and smoothing, Cambridge University Press
+    No control model here.
 
     """
 
-    transition_model: LinearGaussianTransitionModel = Property(
-        doc="The transition model. The :meth:`smooth` function will initially look for a "
-            "transition model in the prediction. If that is not found then this one is used.")
+    transition_model: LinearGaussianTransitionModel = Property(doc="The transition model to be used.")
 
     def _prediction(self, state):
         """ Return the predicted state, either from the prediction directly, or from the attached
@@ -75,28 +40,18 @@ class KalmanSmoother(Smoother):
         if isinstance(state, GaussianStatePrediction):
             return state
         elif isinstance(state, GaussianStateUpdate):
-            if isinstance(state.hypothesis, MultipleHypothesis):
-                predictions = {hypothesis.prediction for hypothesis in state.hypothesis}
-                if len(predictions) == 1:
-                    # One predictions, this is fine to use.
-                    return predictions.pop()
-                else:
-                    # Multiple predictions, so can't process this.
-                    raise ValueError(
-                        "Track has MultipleHypothesis updates with multiple predictions.")
-            else:
-                return state.hypothesis.prediction
+            return state.hypothesis.prediction
         else:
             raise TypeError("States must be GaussianStatePredictions or GaussianStateUpdates.")
 
-    def _transition_model(self, prediction):
+    def _transition_model(self, state):
         """ If it exists, return the transition model from the prediction associated with input
         state. If that doesn't exist then use the (static) transition model defined by the
         smoother.
 
         Parameters
         ----------
-        prediction : :class:`~.GaussianStatePrediction` or :class:`~.GaussianStateUpdate`
+        state : :class:`~.GaussianStatePrediction` or :class:`~.GaussianStateUpdate`
 
         Returns
         -------
@@ -104,52 +59,14 @@ class KalmanSmoother(Smoother):
             The transition model to be associated with state
         """
         # Is there a transition model linked to the prediction?
-        transition_model = getattr(prediction, "transition_model", None)
-        if transition_model is None:
+        if hasattr(self._prediction(state), "transition_model"):
+            transition_model = self._prediction(state).transition_model
+        else:  # No? Return the class attribute
             transition_model = self.transition_model
 
         return transition_model
 
-    def _transition_matrix(self, state, transition_model, **kwargs):
-        """ Return the transition matrix
-
-        Parameters
-        ----------
-        state : :class:`~.State`
-            The input state (to check for a linked prediction)
-        transition_model : :class:`~.TransitionModel`
-            The transition model to be applied to state
-        **kwargs
-            These are passed to the :meth:`matrix()` function
-
-        Returns
-        -------
-         : :class:`numpy.ndarray`
-            The transition matrix
-        """
-        return transition_model.matrix(**kwargs)
-
-    def _smooth_gain(self, state, prediction, **kwargs):
-        """Calculate the smoothing gain
-
-        Parameters
-        ----------
-        state : :class:`~.State`
-            The input state
-        prediction : :class:`~.GaussianStatePrediction`
-            The prediction (from the subsequent state)
-
-        Returns
-        -------
-         : Matrix
-            The smoothing gain
-
-        """
-        return state.covar \
-            @ self._transition_matrix(state, self._transition_model(prediction), **kwargs).T \
-            @ np.linalg.inv(prediction.covar)
-
-    def smooth(self, track, **kwargs):
+    def smooth(self, track):
         """
         Perform the backward recursion to smooth the track.
 
@@ -164,144 +81,50 @@ class KalmanSmoother(Smoother):
             Smoothed track
 
         """
-        try:
-            self._prediction(track[0])
-            start = 0
-        except (ValueError, TypeError):
-            start = 1
+        firststate = True
+        smoothed_track = Track()
+        for state in reversed(track):
 
-        subsq_state = track[-1]
-        smoothed_states = [subsq_state]
-        for state in reversed(track[start:-1]):
+            if firststate:
+                prev_state = state
+                smoothed_track.append(state)
+                firststate = False
+            else:
 
-            # Delta t
-            time_interval = subsq_state.timestamp - state.timestamp
+                # Delta t
+                time_interval = prev_state.timestamp - state.timestamp
 
-            # Retrieve the prediction from the subsequent (k+1th) timestep accessed previously
-            prediction = self._prediction(subsq_state)
-            # The smoothing gain, mean and covariance
-            ksmooth_gain = self._smooth_gain(
-                state, prediction, time_interval=time_interval, **kwargs)
-            smooth_mean = state.state_vector + ksmooth_gain @ (subsq_state.state_vector -
-                                                               prediction.state_vector)
-            smooth_covar = state.covar + \
-                ksmooth_gain @ (subsq_state.covar - prediction.covar) @ ksmooth_gain.T
+                # Get the transition model matrix
+                transition_matrix = self._transition_model(state).matrix(time_interval)
 
-            # Create a new type called SmoothedState?
+                ksmooth_gain = state.covar @ transition_matrix.T @ np.linalg.inv(prediction.covar)
+                smooth_mean = state.state_vector + ksmooth_gain @ (prev_state.state_vector -
+                                                                   prediction.state_vector)
+                smooth_covar = state.covar + \
+                    ksmooth_gain @ (prev_state.covar - prediction.covar) @ ksmooth_gain.T
 
-            subsq_state = type(state).from_state(state, smooth_mean, smooth_covar)
+                # Create a new state called SmoothedState?
+                if isinstance(state, Update):
+                    prev_state = Update.from_state(state, smooth_mean, smooth_covar,
+                                                   timestamp=state.timestamp, hypothesis=state.hypothesis)
+                elif isinstance(state, Prediction):
+                    prev_state = Prediction.from_state(state, smooth_mean, smooth_covar,
+                                                       timestamp=state.timestamp)
 
-            smoothed_states.insert(0, subsq_state)
+                smoothed_track.append(prev_state)
 
-        if start == 1:
-            smoothed_states.insert(0, track[0])
+            # Save the predicted mean and covariance for the next (i.e. previous) timestep
+            prediction = self._prediction(state)
 
-        # Deep copy existing track, but avoid copying original states, as this would be super
-        # expensive. This works by informing deepcopy that the smoothed states are the
-        # replacement object for the original track states.
-        smoothed_track = copy.deepcopy(track, {id(track.states): smoothed_states})
+        smoothed_track.reverse()
         return smoothed_track
+
+    def track_smooth(self, *args, **kwargs):
+        pass
 
 
 class ExtendedKalmanSmoother(KalmanSmoother):
-    r""" The extended version of the Kalman smoother. The equations are modified slightly,
-    analogously to the extended Kalman filter,
-
-    .. math::
-
-        \mathbf{x}_{k|k-1} &= f_{k} (\mathbf{x}_{k-1})
-
-        F_k &\approx J_f (\mathbf{x}_{k-1})
-
-    where :math:`J_f (\mathbf{x}_{k-1})` is the Jacobian matrix evaluated at
-    :math:`\mathbf{x}_{k-1}`. The rest of the calculation proceeds as with the Kalman smoother.
-
-    In fact the first equation isn't calculated -- it's presumed to have been undertaken by a
-    filter when building the track; similarly for the predicted covariance. In practice, the only
-    difference between this and the Kalman smoother is in the use of the linearised transition
-    matrix to calculate the smoothing gain.
+    """
 
     """
     transition_model: TransitionModel = Property(doc="The transition model to be used.")
-
-    def _transition_matrix(self, state, transition_model, linearisation_point=None, **kwargs):
-        r"""Returns the transition matrix, a matrix if the model is linear, or
-        approximated as Jacobian otherwise.
-        Parameters
-        ----------
-        state : :class:`~.State`
-            :math:`\mathbf{x}_{k-1}`
-        transition_model : :class:`~.TransitionModel`
-            transition model to be applied to state
-        linearisation_point : :class:`~State`, optional
-            State to linearise over. Default `None` where state will be used.
-        **kwargs : various, optional
-            These are passed to :meth:`~.TransitionModel.matrix` or
-            :meth:`~.TransitionModel.jacobian`
-        Returns
-        -------
-        : :class:`numpy.ndarray`
-            The transition matrix, :math:`F_k`, if linear (i.e.
-            :meth:`TransitionModel.matrix` exists, or
-            :meth:`~.TransitionModel.jacobian` if not)
-        """
-        if isinstance(transition_model, LinearModel):
-            return transition_model.matrix(**kwargs)
-        else:
-            if linearisation_point is None:
-                linearisation_point = state
-            return transition_model.jacobian(linearisation_point, **kwargs)
-
-
-class UnscentedKalmanSmoother(KalmanSmoother):
-    r"""The unscented version of the Kalman filter. As with the parent version of the Kalman
-    smoother, the mean and covariance of the prediction are retrieved from the track. The
-    unscented transform is used to calculate the smoothing gain.
-
-    """
-    transition_model: TransitionModel = Property(doc="The transition model to be used.")
-
-    alpha: float = Property(
-        default=0.5,
-        doc="Primary sigma point spread scaling parameter. Default is 0.5.")
-    beta: float = Property(
-        default=2,
-        doc="Used to incorporate prior knowledge of the distribution. If the "
-            "true distribution is Gaussian, the value of 2 is optimal. "
-            "Default is 2")
-    kappa: float = Property(
-        default=0,
-        doc="Secondary spread scaling parameter. Default is calculated as "
-            "3-Ns")
-
-    def _smooth_gain(self, state, prediction, time_interval, **kwargs):
-        """Calculate the smoothing gain
-
-        Parameters
-        ----------
-        state : :class:`~.State`
-            The input state
-        prediction : :class:`~.GaussianStatePrediction`
-            The prediction from the subsequent timestep
-
-        Returns
-        -------
-         : Matrix
-            The smoothing gain
-
-        """
-        # This ensures that the time interval is correctly applied.
-        transition_function = partial(
-            self._transition_model(prediction).function,
-            time_interval=time_interval)
-
-        # Get the sigma points from the mean and covariance.
-        sigma_point_states, mean_weights, covar_weights = gauss2sigma(
-            state, self.alpha, self.beta, self.kappa)
-
-        # Use the unscented transform to return the cross-covariance
-        _, _, cross_covar, _, _, _ = unscented_transform(
-            sigma_point_states, mean_weights, covar_weights,
-            transition_function)
-
-        return cross_covar @ np.linalg.inv(prediction.covar)
--- stonesoup/smoother/tests/test_kalman.py
+++ stonesoup/smoother/tests/test_kalman.py
@@ -1,40 +1,22 @@
+# -*- coding: utf-8 -*-
 """Tests the various Kalman-based smoothers. This test replicates that the exists/existed in
 test_lineargaussian"""
 
-import pytest
 import numpy as np
 from datetime import datetime, timedelta
 
 from stonesoup.types.detection import Detection
-from stonesoup.types.multihypothesis import MultipleHypothesis
 from stonesoup.types.state import GaussianState
-from stonesoup.types.prediction import GaussianStatePrediction
 from stonesoup.types.track import Track
 from stonesoup.types.hypothesis import SingleHypothesis
 from stonesoup.models.transition.linear import ConstantVelocity
 from stonesoup.models.measurement.linear import LinearGaussian
 from stonesoup.predictor.kalman import KalmanPredictor
-from stonesoup.types.update import GaussianStateUpdate
 from stonesoup.updater.kalman import KalmanUpdater
-from stonesoup.smoother.kalman import KalmanSmoother, ExtendedKalmanSmoother, \
-    UnscentedKalmanSmoother
+from stonesoup.smoother.kalman import KalmanSmoother
 
 
-@pytest.fixture(
-    params=[
-        KalmanSmoother,  # Standard Kalman
-        ExtendedKalmanSmoother,  # Extended Kalman
-        UnscentedKalmanSmoother,  # Unscented Kalman
-    ],
-    ids=["standard", "extended", "unscented"]
-)
-def smoother_class(request):
-    return request.param
-
-
-@pytest.mark.parametrize("multi_hypothesis", [False, True],
-                         ids=["SingleHypothesis", "MultipleHypothesis"])
-def test_kalman_smoother(smoother_class, multi_hypothesis):
+def test_kalman_smoother():
 
     # First create a track from some detections and then smooth - check the output.
 
@@ -72,11 +54,9 @@ def test_kalman_smoother(smoother_class, multi_hypothesis):
         # Update
         cstate = updater.update(hypothesis)
         # write to track
-        if multi_hypothesis:
-            cstate.hypothesis = MultipleHypothesis([hypothesis])
         track.append(cstate)
 
-    smoother = smoother_class(transition_model=trans_model)
+    smoother = KalmanSmoother(transition_model=trans_model)
     smoothed_track = smoother.smooth(track)
     smoothed_state_vectors = [
         state.state_vector for state in smoothed_track]
@@ -91,48 +71,3 @@ def test_kalman_smoother(smoother_class, multi_hypothesis):
     ]
 
     assert np.allclose(smoothed_state_vectors, target_smoothed_vectors)
-
-    # Check that a prediction is smoothable and that no error chucked
-    # Also remove the transition model and use the one provided by the smoother
-    track[1] = GaussianStatePrediction(pred.state_vector, pred.covar, timestamp=pred.timestamp)
-    smoothed_track2 = smoother.smooth(track)
-    assert isinstance(smoothed_track2[1], GaussianStatePrediction)
-
-    # Check appropriate error chucked if not GaussianStatePrediction/Update
-    track[-1] = detections[-1]
-    with pytest.raises(TypeError):
-        smoother._prediction(track[-1])
-
-
-def test_multi_prediction_exception(smoother_class):
-    """Check error raised with multi-hypothesis with multiple predictions"""
-    start = datetime.now()
-    track = Track()
-    track.states = [
-        GaussianStateUpdate(
-            state_vector=[1],
-            covar=[[1]],
-            timestamp=start,
-            hypothesis=MultipleHypothesis([
-                SingleHypothesis(
-                    GaussianStatePrediction([1], [[1]], timestamp=start), None),
-                SingleHypothesis(
-                    GaussianStatePrediction([2], [[1]], timestamp=start), None),
-                ])
-        ),
-        GaussianStateUpdate(
-            state_vector=[1],
-            covar=[[1]],
-            timestamp=start+timedelta(1),
-            hypothesis=MultipleHypothesis([
-                SingleHypothesis(
-                    GaussianStatePrediction([1], [[1]], timestamp=start+timedelta(1)), None),
-                SingleHypothesis(
-                    GaussianStatePrediction([2], [[1]], timestamp=start+timedelta(1)), None),
-            ])
-        )
-    ]
-    smoother = smoother_class(transition_model=ConstantVelocity(1))
-    with pytest.raises(
-            ValueError, match="Track has MultipleHypothesis updates with multiple predictions"):
-        smoother._prediction(track[0])
