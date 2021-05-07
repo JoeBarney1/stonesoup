b767f8164f8270aa6586d8c958cd30fbc502aa40
idorrington-dstl <idorrington@dstl.gov.uk>
2021-05-07 11:06:25 +0100

    Added random seed to gaussian model. The random seed creates a numpy randomstate, which is then used to generate all subsequent random numbers.

--- stonesoup/models/base.py
+++ stonesoup/models/base.py
@@ -1,10 +1,10 @@
 from abc import abstractmethod
-from typing import TYPE_CHECKING, Union
+from typing import TYPE_CHECKING, Union, Optional
 
 import numpy as np
 from scipy.stats import multivariate_normal
 
-from ..base import Base
+from ..base import Base, Property
 from ..functions import jacobian as compute_jac
 from ..types.array import StateVector, StateVectors, CovarianceMatrix
 from ..types.numeric import Probability
@@ -199,7 +199,7 @@ class GaussianModel(Model):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.random_state = np.random.RandomState(self.seed) if self.seed is not None else None
+        self.random_state = np.random.RandomState(self.seed)
 
     def rvs(self, num_samples: int = 1, random_state=None, **kwargs) ->\
             Union[StateVector, StateVectors]:
@@ -237,119 +237,6 @@ class GaussianModel(Model):
 
         noise = multivariate_normal.rvs(
             np.zeros(self.ndim), covar, num_samples, random_state=random_state)
-
-        noise = np.atleast_2d(noise)
-
-        if self.ndim > 1:
-            noise = noise.T  # numpy.rvs method squeezes 1-dimensional matrices to integers
-
-        if num_samples == 1:
-            return noise.view(StateVector)
-        else:
-            return noise.view(StateVectors)
-
-    def pdf(self, state1: State, state2: State, **kwargs) -> Union[Probability, np.ndarray]:
-        r"""Model pdf/likelihood evaluation function
-
-        Evaluates the pdf/likelihood of ``state1``, given the state
-        ``state2`` which is passed to :meth:`function()`.
-
-        In mathematical terms, this can be written as:
-
-        .. math::
-
-            p = p(y_t | x_t) = \mathcal{N}(y_t; x_t, Q)
-
-        where :math:`y_t` = ``state_vector1``, :math:`x_t` = ``state_vector2``
-        and :math:`Q` = :attr:`covar`.
-
-        Parameters
-        ----------
-        state1 : State
-        state2 : State
-
-        Returns
-        -------
-        : :class:`~.Probability` or :class:`~.numpy.ndarray` of :class:`~.Probability`
-            The likelihood of ``state1``, given ``state2``
-        """
-        return Probability.from_log_ufunc(self.logpdf(state1, state2, **kwargs))
-
-    def logpdf(self, state1: State, state2: State, **kwargs) -> Union[float, np.ndarray]:
-        r"""Model log pdf/likelihood evaluation function
-
-        Evaluates the pdf/likelihood of ``state1``, given the state
-        ``state2`` which is passed to :meth:`function()`.
-
-        In mathematical terms, this can be written as:
-
-        .. math::
-
-            p = p(y_t | x_t) = \mathcal{N}(y_t; x_t, Q)
-
-        where :math:`y_t` = ``state_vector1``, :math:`x_t` = ``state_vector2``
-        and :math:`Q` = :attr:`covar`.
-
-        Parameters
-        ----------
-        state1 : State
-        state2 : State
-
-        Returns
-        -------
-        :  float or :class:`~.numpy.ndarray`
-            The log likelihood of ``state1``, given ``state2``
-        """
-        covar = self.covar(**kwargs)
-
-        # If model has None-type covariance or contains None, it does not represent a Gaussian
-        if covar is None or None in covar:
-            raise ValueError("Cannot generate pdf from None-type covariance")
-
-        # Calculate difference before to handle custom types (mean defaults to zero)
-        # This is required as log pdf coverts arrays to floats
-        likelihood = np.atleast_1d(
-            multivariate_normal.logpdf((state1.state_vector - self.function(state2, **kwargs)).T,
-                                       cov=covar))
-
-        if len(likelihood) == 1:
-            likelihood = likelihood[0]
-
-        return likelihood
-
-    def rvs(self, num_samples: int = 1, **kwargs) -> Union[StateVector, StateVectors]:
-        r"""Model noise/sample generation function
-
-        Generates noise samples from the model.
-
-        In mathematical terms, this can be written as:
-
-        .. math::
-
-            v_t \sim \mathcal{N}(0,Q)
-
-        where :math:`v_t =` ``noise`` and :math:`Q` = :attr:`covar`.
-
-        Parameters
-        ----------
-        num_samples: scalar, optional
-            The number of samples to be generated (the default is 1)
-
-        Returns
-        -------
-        noise : 2-D array of shape (:attr:`ndim`, ``num_samples``)
-            A set of Np samples, generated from the model's noise
-            distribution.
-        """
-
-        covar = self.covar(**kwargs)
-
-        # If model has None-type covariance or contains None, it does not represent a Gaussian
-        if covar is None or None in covar:
-            raise ValueError("Cannot generate rvs from None-type covariance")
-
-        noise = multivariate_normal.rvs(
-            np.zeros(self.ndim), covar, num_samples)
 
         noise = np.atleast_2d(noise)
 
