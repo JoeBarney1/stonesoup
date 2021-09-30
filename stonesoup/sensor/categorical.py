0765e0d6a02d61220351311a769180f025d14f0a
Oliver Harrald <oharrald@dstl.gov.uk>
2021-09-30 14:14:16 +0100

    Add categorical sensor and fix categorical predictor property documentation

--- stonesoup/sensor/categorical.py
+++ stonesoup/sensor/categorical.py
@@ -1,22 +1,19 @@
-from scipy.stats import multinomial
+# -*- coding: utf-8 -*-
+from typing import Set, Sequence
 
 from ..base import Property
-from ..models.measurement.categorical import MarkovianMeasurementModel
+from ..models.measurement.categorical import CategoricalMeasurementModel
 from ..sensor.sensor import Sensor
-from ..types.array import StateVector
-from ..types.detection import TrueCategoricalDetection
+from ..types.detection import TrueDetection, TrueCategoricalDetection
+from ..types.groundtruth import GroundTruthState, GroundTruthPath
+from ..types.state import CategoricalState
 
 
-class HMMSensor(Sensor):
-    r"""Sensor model that observes a categorical state space and returns categorical measurements.
-
-    Measurements are categorical distributions over a finite set of categories
-    :math:`Z = \{\zeta^n|n\in \mathbf{N}, n\le N\}` (for some finite :math:`N`).
-    """
-
-    measurement_model: MarkovianMeasurementModel = Property(
-        doc="Measurement model to generate detection vectors from"
-    )
+class CategoricalSensor(Sensor):
+    measurement_model: CategoricalMeasurementModel = Property(
+        doc="Categorical measurement model used in generating measurements.")
+    category_names: Sequence[str] = Property(default=None,
+                                             doc="Measurement category names.")
 
     @property
     def ndim_state(self):
@@ -26,20 +23,20 @@ class HMMSensor(Sensor):
     def ndim_meas(self):
         return self.measurement_model.ndim_meas
 
-    def measure(self, ground_truths, noise: bool = True, **kwargs):
-        r"""Generate a categorical measurement for a given set of true categorical state.
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if self.category_names and len(self.category_names) != self.ndim_meas:
+            raise ValueError(f"{len(self.category_names)} category names were given for a sensor "
+                             f"which returns vectors of length {self.ndim_meas}")
+
+    def measure(self, ground_truths: Set[GroundTruthState], **kwargs) -> Set[TrueDetection]:
+        """Generate a categorical measurement for a given categorical state.
 
         Parameters
         ----------
-        ground_truths: Set[:class:`~.CategoricalGroundTruthState`]
-            A set of :class:`~.CategoricalGroundTruthState`.
-        noise: bool
-            Indicates whether measurement vectors are sampled from and the resultant measurement
-            categories returned instead. These are discrete categories instead of a distribution
-            over the measurement space. They are represented by N-tuples, with all components
-            equal to 0, except at an index corresponding to the relevant category.
-            For example :math:`e^k` indicates that the measurement category is :math:`\zeta^k`.
-            If `False`, the resultant distribution is returned.
+        ground_truths : Set[:class:`~.CategoricalGroundTruthState`]
+            A set of :class:`~.CategoricalGroundTruthState`
 
         Returns
         -------
@@ -51,23 +48,24 @@ class HMMSensor(Sensor):
         """
 
         detections = set()
-
         for truth in ground_truths:
-            timestamp = truth.timestamp
-            detection_vector = self.measurement_model.function(truth, noise=noise, **kwargs)
 
-            if noise:
-                # Sample from resultant distribution
-                rv = multinomial(n=1, p=detection_vector.flatten())
-                detection_vector = StateVector(rv.rvs(size=1, random_state=None))
+            wrong_type = False
+            if isinstance(truth, GroundTruthPath):
+                if not isinstance(truth[-1], CategoricalState):
+                    wrong_type = True
+            elif not isinstance(truth, CategoricalState):
+                wrong_type = True
 
-            detection = TrueCategoricalDetection(
-                state_vector=detection_vector,
-                timestamp=timestamp,
-                categories=self.measurement_model.measurement_categories,
-                measurement_model=self.measurement_model,
-                groundtruth_path=truth
-            )
+            if wrong_type:
+                raise ValueError("Categorical sensor can only observe categorical states")
+
+            measurement_vector = self.measurement_model.function(truth, **kwargs)
+            detection = TrueCategoricalDetection(state_vector=measurement_vector,
+                                                 timestamp=truth.timestamp,
+                                                 measurement_model=self.measurement_model,
+                                                 groundtruth_path=truth,
+                                                 category_names=self.category_names)
             detections.add(detection)
 
         return detections
