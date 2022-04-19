a4d4bf83f356a1ef4facd54873763fb4b81fab89
john.hiles <john.hiles@wright.edu>
2022-04-19 14:37:13 -0400

    Fixing error in merge.

--- stonesoup/types/state.py
+++ stonesoup/types/state.py
@@ -625,9 +625,6 @@ class EnsembleState(Type):
             
 State.register(EnsembleState)
 
-        """Return the name of the most likely category."""
-        return self.categories[np.argmax(self.state_vector)]
-
 
 class CompositeState(Type):
     """Composite state type.
@@ -707,4 +704,3 @@ class CompositeState(Type):
 
 
 State.register(CompositeState)  # noqa: E305
->>>>>>> 468cfcc32935aad3b7b9d44e43f517914987d17f
