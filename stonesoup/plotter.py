66ff85c1d2c0ee5ccb5b56a794e174459a57b50b
jwright2 <jwright2@dstl.gov.uk>
2022-06-20 09:44:20 +0100

    Add equal axes method for 3d projections.

--- stonesoup/plotter.py
+++ stonesoup/plotter.py
@@ -8,6 +8,8 @@ from matplotlib.patches import Ellipse
 from matplotlib.legend_handler import HandlerPatch
 
 from .types import detection
+from .types.state import State
+from .types.groundtruth import GroundTruthPath
 from .models.base import LinearModel, Model
 
 from enum import Enum
@@ -366,41 +368,40 @@ class Plotter:
         else:
             self.ax.legend(handles=self.legend_dict.values(), labels=self.legend_dict.keys())
 
-    def plot_sensors(self, sensors, sensor_label="Sensor", **kwargs):
-        """Plots sensor(s)
-
-        Plots sensors.  Users can change the color and marker of detections using keyword
-        arguments. Default is a black 'x' marker.
+    def set_equal_3daxis(self, axes=None):
+        """Plots minimum/maximum points with no linestyle to increase the plotting region to
+        simulate `.ax.axis('equal')` from matplotlib 2d plots which is not possible using 3d
+        projection.
 
         Parameters
         ----------
-        sensors : list of :class:`~.Sensor`
-            ~~~
-        \\*\\*kwargs: dict
-            Additional arguments to be passed to plot function for detections. Defaults are
-            ``marker='x'`` and ``color='black'``.
+        axes: list
+            List of dimension index specifying the equal axes, equal x and y = [0,1].
+            Default is x,y [0,1].
         """
+        if not axes:
+            axes = [0, 1]
+        if self.dimension is Dimension.THREE:
+            min_xyz = [0, 0, 0]
+            max_xyz = [0, 0, 0]
+            for n in range(3):
+                for line in self.ax.lines:
+                    min_xyz[n] = np.min([min_xyz[n], *line.get_data_3d()[n]])
+                    max_xyz[n] = np.max([max_xyz[n], *line.get_data_3d()[n]])
 
-        sensor_kwargs = dict(marker='x', color='black')
-        sensor_kwargs.update(kwargs)
+            extremes = np.max([x - y for x, y in zip(max_xyz, min_xyz)])
+            equal_axes = [0, 0, 0]
+            for i in axes:
+                equal_axes[i] = 1
+            lower = ([np.mean([x, y]) for x, y in zip(max_xyz, min_xyz)] - extremes/2) * equal_axes
+            upper = ([np.mean([x, y]) for x, y in zip(max_xyz, min_xyz)] + extremes/2) * equal_axes
+            ghosts = GroundTruthPath(states=[State(state_vector=lower),
+                                             State(state_vector=upper)])
 
-        if not isinstance(sensors, set):
-            sensors = {sensors}  # Make a set of length 1
-
-        for sensor in sensors:
-            if self.dimension is Dimension.TWO:  # plots the sensors in xy
-                self.ax.scatter(sensor.position[0],
-                                sensor.position[1],
-                                **sensor_kwargs)
-            elif self.dimension is Dimension.THREE:  # plots the sensors in xyz
-                self.ax.plot3D(sensor.position[0],
-                               sensor.position[1],
-                               sensor.position[2],
-                               **sensor_kwargs)
-            else:
-                raise NotImplementedError('Unsupported dimension type for sensor plotting')
-        self.legend_dict[sensor_label] = Line2D([], [], linestyle='', **sensor_kwargs)
-        self.ax.legend(handles=self.legend_dict.values(), labels=self.legend_dict.keys())
+            self.ax.plot3D([state.state_vector[0] for state in ghosts],
+                           [state.state_vector[1] for state in ghosts],
+                           [state.state_vector[2] for state in ghosts],
+                           linestyle="")
 
     # Ellipse legend patch (used in Tutorial 3)
     @staticmethod
