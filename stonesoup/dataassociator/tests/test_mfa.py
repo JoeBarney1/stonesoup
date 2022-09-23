1fc223739926833e9b0716c155fa9169954ab886
Steven Hiscocks <sdhiscocks@dstl.gov.uk>
2022-09-23 11:42:30 +0100

    Add test for MFA data associator

--- stonesoup/dataassociator/tests/test_mfa.py
+++ stonesoup/dataassociator/tests/test_mfa.py
@@ -4,19 +4,16 @@ from itertools import product
 import numpy as np
 import pytest
 
+from ..mfa import MFADataAssociator
 from ...gater.distance import DistanceGater
 from ...hypothesiser.mfa import MFAHypothesiser
 from ...measures import Mahalanobis
 from ...types.detection import Detection
 from ...types.mixture import GaussianMixture
 from ...types.numeric import Probability
-from ...types.state import TaggedWeightedGaussianState, GaussianState
+from ...types.state import TaggedWeightedGaussianState
 from ...types.track import Track
 from ...types.update import GaussianMixtureUpdate
-try:
-    from ..mfa import MFADataAssociator
-except ImportError:
-    pytest.skip("ortools not available", allow_module_level=True)
 
 
 def update_tracks(associations, updater):
@@ -34,26 +31,19 @@ def update_tracks(associations, updater):
 def generate_detections(tracks, timestamp, predictor, measurement_model, n=2):
     return {
         Detection(
-            measurement_model.function(predictor.predict(
-                GaussianState(
-                    track.mean,
-                    track.covar,
-                    track.timestamp), timestamp), noise=True),
+            measurement_model.function(predictor.predict(track, timestamp), noise=True),
             timestamp=timestamp)
         for track in tracks for _ in range(n)}  # n detections per track; pseudo clutter
 
 
-@pytest.fixture(scope='function', params=[2, 3, 6])
-def data_associator(request, probability_hypothesiser):
+@pytest.mark.parametrize('slide_window', (2, 3, 6))
+def test_mfa(predictor, updater, measurement_model, probability_hypothesiser, slide_window):
+    start_time = datetime.datetime.now()
+
     # Hypothesiser and Data Associator
     hypothesiser = MFAHypothesiser(probability_hypothesiser)
 
-    return MFADataAssociator(hypothesiser, slide_window=request.param)
-
-
-def test_mfa(predictor, updater, measurement_model, data_associator):
-    start_time = datetime.datetime.now()
-    slide_window = data_associator.slide_window
+    data_associator = MFADataAssociator(hypothesiser, slide_window=slide_window)
 
     prior1 = GaussianMixture([TaggedWeightedGaussianState([[0], [1], [0], [1]],
                                                           np.diag([1.5, 0.5, 1.5, 0.5]),
@@ -133,11 +123,3 @@ def test_mfa(predictor, updater, measurement_model, data_associator):
                 assert tuple(hyp.prediction.tag) in list(product(range(0, 5),
                                                                  range(0, 5),
                                                                  range(1, 5)))
-
-
-def test_mfa_no_tracks(data_associator):
-    associations = data_associator.associate(set(), set(), datetime.datetime.now())
-    assert not associations
-
-    associations = data_associator.associate(set(), {Detection([0, 1])}, datetime.datetime.now())
-    assert not associations
