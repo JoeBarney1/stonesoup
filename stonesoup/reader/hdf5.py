e2d874d994411b04682378c7d91ca17124480bc5
cpbrophy <64288057+cpbrophy@users.noreply.github.com>
2022-09-29 10:23:20 -0400

    Implement generic readers for HDF5
    Borrowed from the generic CSV readers to support HDF5 files, adding the option to reduce the resolution of time fields while reading data in.

--- stonesoup/reader/hdf5.py
+++ stonesoup/reader/hdf5.py
@@ -5,12 +5,12 @@ of data that is in `HDF5 <https://hdfgroup.org/>`_ format, using the `h5py
 <https://docs.h5py.org/>`_ library.
 """
 
-from datetime import datetime, timedelta, timezone
+from datetime import datetime, timedelta
 from typing import Collection, Sequence
 
 try:
     import h5py
-except ImportError as error:  # pragma: no cover
+except ImportError as error:
     raise ImportError(
         "HDF5 Readers require the dependency 'h5py' to be installed."
     ) from error
@@ -122,8 +122,7 @@ class _HDF5Reader(BinaryFileReader):
         if self.time_field_format is not None:
             time_field_value = datetime.strptime(raw_time_val, self.time_field_format)
         elif self.timestamp is True:
-            time_field_value = datetime.fromtimestamp(
-                raw_time_val, timezone.utc).replace(tzinfo=None)
+            time_field_value = datetime.utcfromtimestamp(raw_time_val)
         else:
             time_field_value = parse(raw_time_val, ignoretz=True)
 
