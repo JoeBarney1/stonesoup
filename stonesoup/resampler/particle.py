366a57609d6bc7f46c980824fec83bababaa458e
Steven Hiscocks <SDHISCOCKS@dstl.gov.uk>
2023-03-16 16:20:14 +0000

    Add log_weights to ParticleState
    This may have performance advantage by simply storing weights in log
    space, and converting to weights dynamically when requried (utilising a
    cache for the property).
    
    Changes here are mostly backwards compatible, other than an issue with
    `from_state` method.

--- stonesoup/resampler/particle.py
+++ stonesoup/resampler/particle.py
@@ -2,7 +2,6 @@ import numpy as np
 
 from .base import Resampler
 from ..base import Property
-from ..types.numeric import Probability
 from ..types.state import ParticleState
 
 
@@ -26,12 +25,9 @@ class SystematicResampler(Resampler):
 
         if not isinstance(particles, ParticleState):
             particles = ParticleState(None, particle_list=particles)
-        if nparts is None:
-            nparts = len(particles)
+        n_particles = len(particles)
 
-        weight = Probability(1 / nparts)
-
-        log_weights = np.asfarray(np.log(particles.weight))
+        log_weights = particles.log_weight
         weight_order = np.argsort(log_weights, kind='stable')
         max_log_value = log_weights[weight_order[-1]]
         with np.errstate(divide='ignore'):
@@ -47,8 +43,7 @@ class SystematicResampler(Resampler):
         index = weight_order[np.searchsorted(cdf, np.log(u_j))]
 
         new_particles = particles[index]
-        new_particles.weight = np.full((nparts, ), weight)
-
+        new_particles.log_weight = np.full((n_particles, ), np.log(1/n_particles))
         return new_particles
 
 
@@ -89,7 +84,8 @@ class ESSResampler(Resampler):
             particles = ParticleState(None, particle_list=particles)
         if self.threshold is None:
             self.threshold = len(particles) / 2
-        if 1 / np.sum(np.square(particles.weight)) < self.threshold:  # If ESS too small, resample
+        # If ESS too small, resample
+        if 1 / np.sum(np.exp(2*particles.log_weight)) < self.threshold:
             return self.resampler.resample(self.resampler, particles)
         else:
             return particles
