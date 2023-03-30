3b09f278d5ce32ed9ffa22cb5f72e410e1804e16
Timothy Glover <t.glover@lboro.ac.uk>
2023-03-30 13:12:31 +0100

    Created, and tested, regulariser object and MCMCRegulariser class for particle regularisation.

--- stonesoup/regulariser/particle.py
+++ stonesoup/regulariser/particle.py
@@ -1,13 +1,9 @@
 import copy
 import numpy as np
 from scipy.stats import multivariate_normal, uniform
-from typing import Sequence, Callable
 
 from .base import Regulariser
 from ..functions import cholesky_eps
-from ..types.state import ParticleState
-from ..models.transition import TransitionModel
-from ..base import Property
 
 
 class MCMCRegulariser(Regulariser):
@@ -17,76 +13,55 @@ class MCMCRegulariser(Regulariser):
     of effectiveness. Sometimes this is not desirable, or possible, when a particular algorithm
     requires the introduction of new samples as part of the filtering process for example.
 
-    This is a particular implementation of a MCMC move step that uses the Metropolis-Hastings
-    algorithm [1]_. After resampling, particles are moved a small amount, according do a Gaussian
-    kernel, to a new state only if the Metropolis-Hastings acceptance probability is met by a
+    This is a particlar implementation of a MCMC move step that uses the Metropolis-Hastings
+    algorithm [1]. After resampling, particles are moved a small amount, according do a Gaussian
+    kernal, to a new state only if the Metropolis-Hastings acceptance probability is met by a
     random number assigned to each particle from a uniform random distribution, otherwise they
-    remain the same. Further details on the implementation are given in [2]_.
+    remain the same. Further details on the implementation are given in [2].
 
     References
     ----------
     .. [1] Robert, Christian P. & Casella, George, Monte Carlo Statistical Methods, Springer, 1999.
 
-    .. [2] Ristic, Branko & Arulampalam, Sanjeev & Gordon, Neil, Beyond the Kalman Filter:
+    .. [2] Ristic, Branco & Arulampalam, Sanjeev & Gordon, Neil, Beyond the Kalman Filter:
         Particle Filters for Target Tracking Applications, Artech House, 2004. """
 
-    transition_model: TransitionModel = Property(doc="Transition model used for prediction",
-                                                 default=None)
-    constraint_func: Callable = Property(
-        default=None,
-        doc="Callable, user defined function for applying "
-            "constraints to particle states. This is done by reverting particles "
-            "that are moved to a state outside of the defined constraints "
-            "back to the state prior to the move step. Particle states that are "
-            "input are assumed to be constrained. This function provides indices "
-            "of the unconstrained particles and should accept a :class:`~.ParticleState` "
-            "object and return an array-like object of logical indices. "
-    )
-
-    def regularise(self, prior, posterior):
+    def regularise(self, prior, posterior, detections):
         """Regularise the particles
 
         Parameters
         ----------
         prior : :class:`~.ParticleState` type
-            prior particle distribution.
+            prior particle distribution
         posterior : :class:`~.ParticleState` type
-            posterior particle distribution.
+            posterior particle distribution
+        detections : set of :class:`~.Detection`
+            set of detections containing clutter,
+            true detections or both
 
         Returns
         -------
         particle state: :class:`~.ParticleState`
            The particle state after regularisation
         """
-
-        if not isinstance(posterior, ParticleState):
-            raise TypeError('Only ParticleState type is supported!')
-
-        if not isinstance(prior, ParticleState):
-            raise TypeError('Only ParticleState type is supported!')
+        posterior = copy.copy(posterior)
+        timestamp = posterior.timestamp
+        if posterior.state_vector is None:
+            posterior = posterior.particle_list
+            posterior.timestamp = timestamp
+        if prior.state_vector is None:
+            prior = prior.particle_list
 
         regularised_particles = copy.copy(posterior)
         moved_particles = copy.copy(posterior)
-        transitioned_prior = copy.copy(prior)
 
-        hypotheses = posterior.hypothesis if isinstance(posterior.hypothesis, Sequence) \
-            else [posterior.hypothesis]
-
-        transition_model = hypotheses[0].prediction.transition_model or self.transition_model
-        if transition_model is not None:
-            time_interval = posterior.timestamp - prior.timestamp
-            transitioned_prior.state_vector = \
-                transition_model.function(prior, noise=False, time_interval=time_interval)
-
-        detections = {hypothesis.measurement for hypothesis in hypotheses if hypothesis}
-
-        if detections:
+        if detections is not None:
             ndim = prior.state_vector.shape[0]
             nparticles = len(posterior)
 
             measurement_model = next(iter(detections)).measurement_model
 
-            # calculate the optimal bandwidth for the Gaussian kernel
+            # calculate the optimal bandwidth for the Gaussian kernal
             hopt = (4/(ndim+2))**(1/(ndim+4))*nparticles**(-1/(ndim+4))
             covar_est = posterior.covar
 
@@ -94,35 +69,32 @@ class MCMCRegulariser(Regulariser):
             moved_particles.state_vector = moved_particles.state_vector + \
                 hopt * cholesky_eps(covar_est) @ np.random.randn(ndim, nparticles)
 
-            # Apply constraints if defined
-            if self.constraint_func is not None:
-                part_indx = self.constraint_func(moved_particles)
-                moved_particles.state_vector[:, part_indx] = posterior.state_vector[:, part_indx]
-
             # Evaluate likelihoods
-            part_diff = moved_particles.state_vector - transitioned_prior.state_vector
-            move_likelihood = multivariate_normal.logpdf(part_diff.T,
-                                                         cov=covar_est)
-            post_part_diff = posterior.state_vector - transitioned_prior.state_vector
-            post_likelihood = multivariate_normal.logpdf(post_part_diff.T,
-                                                         cov=covar_est)
+            part_diff = np.array(moved_particles.state_vector - prior.state_vector).T
+            part_diff_mean = np.mean(part_diff, axis=0)
+            move_likelihood = multivariate_normal.pdf(part_diff,
+                                                      mean=part_diff_mean,
+                                                      cov=np.array(covar_est))
+            post_part_diff = np.array(posterior.state_vector - prior.state_vector).T
+            post_part_diff_mean = np.mean(post_part_diff, axis=0)
+            post_likelihood = multivariate_normal.pdf(post_part_diff,
+                                                      mean=post_part_diff_mean,
+                                                      cov=covar_est)
 
             # Evaluate measurement likelihoods
             move_meas_likelihood = []
             post_meas_likelihood = []
             for detection in detections:
                 move_meas_likelihood.append(measurement_model.logpdf(detection, moved_particles))
-                post_meas_likelihood.append(measurement_model.logpdf(detection, posterior))
+                post_meas_likelihood.append(measurement_model.logpdf(detection, moved_particles))
 
             # In the case that there are multiple measurements,
             # we select the highest overall likelihood.
             max_likelihood_idx = np.argmax(np.sum(move_meas_likelihood, axis=1))
 
             # Calculate acceptance probability (alpha)
-            # with np.errstate(invalid="ignore"):
-            with np.errstate(invalid='ignore', over='ignore'):
-                alpha = np.exp((move_meas_likelihood[max_likelihood_idx] + move_likelihood) -
-                               (post_meas_likelihood[max_likelihood_idx] + post_likelihood))
+            alpha = (move_meas_likelihood[max_likelihood_idx]*move_likelihood) / \
+                    (post_meas_likelihood[max_likelihood_idx]*post_likelihood)
 
             # All 'jittered' particles that are above the alpha threshold are kept, the rest are
             # rejected and the original posterior used
