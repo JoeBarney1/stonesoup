c3598100d85a9b3b3823304c1875d55139b4fbd9
Kenyon Alex J <akenyon@rnet.dstl.gov.uk>
2023-10-24 10:35:24 +0100

    moving test setup to conftest file

--- stonesoup/sensormanager/tests/conftest.py
+++ stonesoup/sensormanager/tests/conftest.py
@@ -58,7 +58,7 @@ def params():
         fov_angle=np.radians(90),
         dwell_centre=StateVector([np.radians(315)]),
         max_range=np.inf,
-        resolution=Angle(np.radians(90))
+        resolutions={'dwell_centre': Angle(np.radians(90))}
     )
     sensor.timestamp = start_time
     sensor_set.add(sensor)
