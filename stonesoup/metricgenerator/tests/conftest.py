c7422ff917160d4decb1261d2b9f01fa6134ebc1
Marat Kopytjuk <kopytjuk@gmail.com>
2024-07-29 22:18:42 +0200

    refactor time-period

--- stonesoup/metricgenerator/tests/conftest.py
+++ stonesoup/metricgenerator/tests/conftest.py
@@ -20,9 +20,14 @@ from ...types.track import Track
 from ...types.update import GaussianStateUpdate
 
 
+@pytest.fixture
+def time_period() -> timedelta:
+    return timedelta(seconds=1)
+
+
 @pytest.fixture()
 def trial_timestamps(time_period: timedelta):
-    now = datetime(2024, 1, 1, 0, 0, 0)
+    now = datetime.now()
     return [now + i*time_period for i in range(4)]
 
 
