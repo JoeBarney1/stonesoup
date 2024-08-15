c8aad285d7f9a774b83898aadd48584424d7f211
G Webb <gawebb@dstl.gov.uk>
2024-08-15 14:27:53 +0100

    Replaced 'measurements_label', 'track_label', 'sensor_label' and 'truths_label' with 'label' in examples and tutorials.

--- docs/examples/plotting/Polar_Plotting.py
+++ docs/examples/plotting/Polar_Plotting.py
@@ -153,9 +153,9 @@ plotter_az_t_cart.fig
 # Azimuth Angle (Degrees) vs Time (s)
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 mapping = [0]
-plotter_az_t = PolarPlotterly(title="Polar - Azimuth Angle (Degrees) vs Time (s)")
-plotter_az_t.plot_ground_truths({angular_ground_truth_1}, mapping=mapping, truths_label="Target 1")
-plotter_az_t.plot_ground_truths({angular_ground_truth_2}, mapping=mapping, truths_label="Target 2")
+plotter_az_t = PolarPlotterly(title="Azimuth Angle (Degrees) vs Time (s)")
+plotter_az_t.plot_ground_truths({angular_ground_truth_1}, mapping=mapping, label="Target 1")
+plotter_az_t.plot_ground_truths({angular_ground_truth_2}, mapping=mapping, label="Target 2")
 plotter_az_t.plot_measurements(detections, mapping=mapping, convert_measurements=False)
 plotter_az_t.fig
 
