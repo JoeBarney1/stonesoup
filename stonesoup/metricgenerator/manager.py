b2a1f66f0ada073cd88cd1368fa0921b3d74cd29
Marat Kopytjuk <kopytjuk@gmail.com>
2024-09-28 16:31:26 +0200

    fix failing tests due to `generator_name`

--- stonesoup/metricgenerator/manager.py
+++ stonesoup/metricgenerator/manager.py
@@ -1,6 +1,6 @@
 from collections.abc import Sequence, Iterable
 from itertools import chain
-from typing import Union
+from typing import Dict, Iterable, Sequence, Union
 
 from ..base import Property
 from ..dataassociator import Associator
@@ -78,7 +78,7 @@ class MultiManager(MetricManager):
             Metrics generated
         """
 
-        metrics: dict[dict] = {}
+        metrics: Dict[Dict] = {}
 
         generators = self.generators if isinstance(self.generators, list) else [self.generators]
 
