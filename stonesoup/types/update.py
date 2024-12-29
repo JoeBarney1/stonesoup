21516af7fadddf46dd9d864ae4893576d807f14d
JoeBarney1 <98259645+JoeBarney1@users.noreply.github.com>
2024-12-29 15:44:50 +0000

    no need to store extra data. removed from updater class, resample index added to update state during resampling

--- stonesoup/types/update.py
+++ stonesoup/types/update.py
@@ -151,7 +151,7 @@ class CompositeUpdate(Update, CompositeState):
 
 
 class MarginalisedParticleStateUpdate(Update, MarginalisedParticleState):
-    extra_data: dict = Property(default=None, doc = "optional dict object to store additional particle information")
+    resample_index: dict = Property(default=None, doc = "optional dict object to store resampling info for back-propagation")
     """RBStateUpdate type
 
     This is a simple MarginalisedParticle state update object.
