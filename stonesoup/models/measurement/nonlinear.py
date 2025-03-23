JPompeus/Stone-Soup

Add a combined non-linear Gaussian measurement model
This allows multiple measurement models to be combined together, in
particular for combining a non-linear model with one or more linear or
non-linear models.

stonesoup/models/measurement/nonlinear.py
@@ -1,17 +1,95 @@
 # -*- coding: utf-8 -*-
 
 import scipy as sp
-from numpy.linalg import inv
+from scipy.linalg import inv, block_diag
 
 from ...base import Property
 
 from ...functions import cart2pol, pol2cart, \
     cart2sphere, sphere2cart, cart2angles, \
     rotx, roty, rotz
-from ...types.array import StateVector, CovarianceMatrix
+from ...types.array import StateVector, CovarianceMatrix, Matrix
 from ...types.angle import Bearing, Elevation
-from ..base import NonLinearModel, GaussianModel, ReversibleModel
+from ..base import LinearModel, NonLinearModel, GaussianModel, ReversibleModel
 from .base import MeasurementModel
+
+
+class CombinedReversibleGaussianMeasurementModel(
+        ReversibleModel, GaussianModel, MeasurementModel):
+    r"""Combine multiple models into a single model by stacking them.
+
+    The assumption is that all models are Gaussian, and must be combination of
+    :class:`~.LinearModel` and :class:`~.NonLinearModel` models. They must all
+    expect the same dimension state vector (i.e. have the same
+    :attr:`~.MeasurementModel.ndim_state`), using model mapping as appropriate.
+
+    This also implements the :meth:`inverse_function`, but will raise a
+    :exc:`NotImplementedError` if any model isn't either a
+    :class:`~.LinearModel` or :class:`~.ReversibleModel`.
+    """
+    mapping = None
+    model_list = Property(
+        [MeasurementModel], doc="List of Measurement Models.")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for model in self.model_list:
+            if model.ndim_state != self.ndim_state:
+                raise ValueError("Models must all have the same `ndim_state`")
+
+    @property
+    def ndim_state(self):
+        """Number of state dimensions"""
+        return self.model_list[0].ndim_state
+
+    @property
+    def ndim_meas(self):
+        return sum(model.ndim_meas for model in self.model_list)
+
+    def function(self, state_vector, **kwargs):
+        return sp.vstack([model.function(state_vector, **kwargs)
+                          for model in self.model_list]).view(StateVector)
+
+    @staticmethod
+    def _linear_inverse_function(model, meas_vector, **kwargs):
+        model_matrix = model.matrix(**kwargs)
+        inv_model_matrix = sp.linalg.pinv(model_matrix)
+
+        return inv_model_matrix@meas_vector
+
+    def inverse_function(self, meas_vector, **kwargs):
+        ndim_count = 0
+        state_vector = sp.zeros((self.ndim_state, 1)).view(StateVector)
+        for model in self.model_list:
+            if isinstance(model, ReversibleModel):
+                state_vector += model.inverse_function(
+                    meas_vector[ndim_count:model.ndim_meas+ndim_count, :],
+                    **kwargs)
+            elif isinstance(model, LinearModel):
+                state_vector += self._linear_inverse_function(
+                    model,
+                    meas_vector[ndim_count:model.ndim_meas+ndim_count, :],
+                    **kwargs)
+            else:
+                raise NotImplementedError(
+                    "Model {!r} not reversible".format(type(model)))
+            ndim_count += model.ndim_meas
+
+        return state_vector
+
+    def covar(self, **kwargs):
+        return block_diag(
+            *(model.covar(**kwargs) for model in self.model_list)
+            ).view(CovarianceMatrix)
+
+    def rvs(self, num_samples=1, **kwargs):
+        rvs_vectors = sp.vstack([model.rvs(num_samples, **kwargs)
+                                 for model in self.model_list])
+        if num_samples == 1:
+            return rvs_vectors.view(StateVector)
+        else:
+            return rvs_vectors.view(Matrix)
 
 
 class NonLinearGaussianMeasurement(MeasurementModel,

JoeBarney1/stonesoup

stonesoup/models/measurement/nonlinear.py
@@ -1,17 +1,95 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-from numpy.linalg import inv
+from scipy.linalg import inv, pinv, block_diag
 
 from ...base import Property
 
 from ...functions import cart2pol, pol2cart, \
     cart2sphere, sphere2cart, cart2angles, \
     rotx, roty, rotz
-from ...types.array import StateVector, CovarianceMatrix
+from ...types.array import StateVector, CovarianceMatrix, Matrix
 from ...types.angle import Bearing, Elevation
-from ..base import NonLinearModel, GaussianModel, ReversibleModel
+from ..base import LinearModel, NonLinearModel, GaussianModel, ReversibleModel
 from .base import MeasurementModel
+
+
+class CombinedReversibleGaussianMeasurementModel(
+        ReversibleModel, GaussianModel, MeasurementModel):
+    r"""Combine multiple models into a single model by stacking them.
+
+    The assumption is that all models are Gaussian, and must be combination of
+    :class:`~.LinearModel` and :class:`~.NonLinearModel` models. They must all
+    expect the same dimension state vector (i.e. have the same
+    :attr:`~.MeasurementModel.ndim_state`), using model mapping as appropriate.
+
+    This also implements the :meth:`inverse_function`, but will raise a
+    :exc:`NotImplementedError` if any model isn't either a
+    :class:`~.LinearModel` or :class:`~.ReversibleModel`.
+    """
+    mapping = None
+    model_list = Property(
+        [MeasurementModel], doc="List of Measurement Models.")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for model in self.model_list:
+            if model.ndim_state != self.ndim_state:
+                raise ValueError("Models must all have the same `ndim_state`")
+
+    @property
+    def ndim_state(self):
+        """Number of state dimensions"""
+        return self.model_list[0].ndim_state
+
+    @property
+    def ndim_meas(self):
+        return sum(model.ndim_meas for model in self.model_list)
+
+    def function(self, state_vector, **kwargs):
+        return np.vstack([model.function(state_vector, **kwargs)
+                          for model in self.model_list]).view(StateVector)
+
+    @staticmethod
+    def _linear_inverse_function(model, meas_vector, **kwargs):
+        model_matrix = model.matrix(**kwargs)
+        inv_model_matrix = pinv(model_matrix)
+
+        return inv_model_matrix@meas_vector
+
+    def inverse_function(self, meas_vector, **kwargs):
+        ndim_count = 0
+        state_vector = np.zeros((self.ndim_state, 1)).view(StateVector)
+        for model in self.model_list:
+            if isinstance(model, ReversibleModel):
+                state_vector += model.inverse_function(
+                    meas_vector[ndim_count:model.ndim_meas+ndim_count, :],
+                    **kwargs)
+            elif isinstance(model, LinearModel):
+                state_vector += self._linear_inverse_function(
+                    model,
+                    meas_vector[ndim_count:model.ndim_meas+ndim_count, :],
+                    **kwargs)
+            else:
+                raise NotImplementedError(
+                    "Model {!r} not reversible".format(type(model)))
+            ndim_count += model.ndim_meas
+
+        return state_vector
+
+    def covar(self, **kwargs):
+        return block_diag(
+            *(model.covar(**kwargs) for model in self.model_list)
+            ).view(CovarianceMatrix)
+
+    def rvs(self, num_samples=1, **kwargs):
+        rvs_vectors = np.vstack([model.rvs(num_samples, **kwargs)
+                                 for model in self.model_list])
+        if num_samples == 1:
+            return rvs_vectors.view(StateVector)
+        else:
+            return rvs_vectors.view(Matrix)
 
 
 class NonLinearGaussianMeasurement(MeasurementModel,